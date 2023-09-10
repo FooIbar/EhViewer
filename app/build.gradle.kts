e94424a83e6726c91ed8a51e7bd47f900cf39047
FooIbar <118464521+FooIbar@users.noreply.github.com>
2023-09-10 21:22:18 +0800

    Release 1.8.9.5-gms

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,7 +64,7 @@ android {
         minSdk = 28
         targetSdk = 34
         versionCode = 180043
-        versionName = "1.8.9.4-gms"
+        versionName = "1.8.9.5-gms"
         resourceConfigurations.addAll(
             listOf(
                 "zh",
