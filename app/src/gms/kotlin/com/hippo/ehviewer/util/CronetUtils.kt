e07323235256f6c496c8061a2896af42b618551f
FooIbar <118464521+FooIbar@users.noreply.github.com>
2023-10-22 17:52:16 +0800

    Fix cronet support detection

--- app/src/gms/kotlin/com/hippo/ehviewer/util/CronetUtils.kt
+++ app/src/gms/kotlin/com/hippo/ehviewer/util/CronetUtils.kt
@@ -2,10 +2,11 @@ package com.hippo.ehviewer.util
 
 import com.google.android.gms.net.CronetProviderInstaller
 import com.hippo.ehviewer.Settings
+import org.chromium.net.CronetProvider
 import splitties.init.appCtx
 
 val isCronetSupported: Boolean
-    get() = Settings.enableCronet && CronetProviderInstaller.isInstalled().also {
+    get() = Settings.enableCronet && CronetProvider.getAllProviders(appCtx).any { it.isEnabled }.also {
         if (!it) {
             CronetProviderInstaller.installProvider(appCtx)
         }
