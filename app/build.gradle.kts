ab8671cc3b0a5631954b0b4e21909367de57e11a
FooIbar <118464521+FooIbar@users.noreply.github.com>
2023-10-24 23:53:25 +0800

    Revert "Exclude some libraries from aboutLibraries"
    This reverts commit bc34f25a3cea72b76ec12e867af4f4b80c6e8545.
    
    Reason for revert: Licenses should be included as per Apache 2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -287,16 +287,6 @@ ksp {
 aboutLibraries {
     duplicationMode = MERGE
     duplicationRule = GROUP
-    exclusionPatterns = listOf(
-        "androidx\\..*".toPattern(),
-        ".*annotations".toPattern(),
-        "com\\.google\\.code\\..*".toPattern(),
-        "com\\.google\\.guava:listenablefuture".toPattern(),
-        "com\\.google\\.protobuf:protobuf-javalite".toPattern(),
-        "org\\.checkerframework:checker-qual".toPattern(),
-        "org\\.chromium\\.net:cronet-embedded".toPattern(),
-        "org\\.slf4j:slf4j-api".toPattern(),
-    )
 }
 
 cargo {
