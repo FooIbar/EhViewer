caf96a72bf0a7767d5cd45ed8fce2a88498e5b27
FooIbar <118464521+FooIbar@users.noreply.github.com>
2023-10-30 00:04:38 +0800

    Avoid uncaught exceptions crashing entire app

---
+++ app/src/main/kotlin/eu/kanade/tachiyomi/network/interceptor/UncaughtExceptionInterceptor.kt
@@ -0,0 +1,28 @@
+package eu.kanade.tachiyomi.network.interceptor
+
+import java.io.IOException
+import okhttp3.Interceptor
+import okhttp3.Response
+
+/**
+ * Catches any uncaught exceptions from later in the chain and rethrows as a non-fatal
+ * IOException to avoid catastrophic failure.
+ *
+ * This should be the first interceptor in the client.
+ *
+ * See [Interceptor](https://square.github.io/okhttp/5.x/okhttp/okhttp3/-interceptor/index.html)
+ */
+class UncaughtExceptionInterceptor : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        return try {
+            chain.proceed(chain.request())
+        } catch (e: Exception) {
+            if (e is IOException) {
+                throw e
+            } else {
+                throw IOException(e)
+            }
+        }
+    }
+}
