95d4dc405164d5ad89abd2a8e6507a135cde9af8
Lokesh Gidra <lokeshgidra@google.com>
2023-11-11 20:36:37 +0800

    Navbar should be contentpadding

--- app/src/main/kotlin/com/hippo/ehviewer/ui/scene/SearchBarScene.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/ui/scene/SearchBarScene.kt
@@ -11,9 +11,11 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -262,7 +264,10 @@ fun SearchBarScreen(
                 }
             },
         ) {
-            LazyColumn(modifier = Modifier.navigationBarsPadding().imePadding()) {
+            LazyColumn(
+                modifier = Modifier.imePadding(),
+                contentPadding = WindowInsets.navigationBars.asPaddingValues(),
+            ) {
                 items(mSuggestionList) {
                     ListItem(
                         headlineContent = { Text(text = it.keyword) },
