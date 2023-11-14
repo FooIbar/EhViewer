0b564933a45f478414ee17386101112ea10ba98a
FooIbar <118464521+FooIbar@users.noreply.github.com>
2023-11-14 22:55:24 +0800

    Fix light splash screen in dark theme on API >= 31

--- app/src/main/kotlin/com/hippo/ehviewer/SettingsCollector.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/SettingsCollector.kt
@@ -1,5 +1,6 @@
 package com.hippo.ehviewer
 
+import android.app.UiModeManager
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY
 import androidx.core.os.LocaleListCompat
@@ -8,6 +9,7 @@ import com.hippo.ehviewer.client.EhEngine
 import com.hippo.ehviewer.client.EhTagDatabase
 import com.hippo.ehviewer.dailycheck.updateDailyCheckWork
 import com.hippo.ehviewer.ui.keepNoMediaFileStatus
+import com.hippo.ehviewer.util.isAtLeastS
 import eu.kanade.tachiyomi.util.lang.launchIO
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -17,6 +19,7 @@ import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.launch
 import splitties.init.appCtx
 import splitties.preferences.PrefDelegate
+import splitties.systemservices.uiModeManager
 
 private val collectScope = CoroutineScope(Dispatchers.Main + SupervisorJob())
 fun <T, R : PrefDelegate<T>> R.observed(func: (Unit) -> Unit) = apply { collectScope.launch { changesFlow().collect(func) } }
@@ -44,14 +47,23 @@ fun updateWhenKeepMediaStatusChanges() {
 fun updateWhenThemeChanges() {
     collectScope.launch {
         delay(100) // Avoid recompose being cancelled
-        AppCompatDelegate.setDefaultNightMode(Settings.theme)
+        if (isAtLeastS) {
+            val mode = when (Settings.theme) {
+                AppCompatDelegate.MODE_NIGHT_NO -> UiModeManager.MODE_NIGHT_NO
+                AppCompatDelegate.MODE_NIGHT_YES -> UiModeManager.MODE_NIGHT_YES
+                else -> UiModeManager.MODE_NIGHT_AUTO
+            }
+            uiModeManager.setApplicationNightMode(mode)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(Settings.theme)
+        }
     }
 }
 
 fun updateWhenAmoledModeChanges() {
     collectScope.launch {
         delay(100) // Avoid recompose being cancelled
-        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_AUTO_BATTERY)
+        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_AUTO_BATTERY) // Trigger onNightModeChanged
         AppCompatDelegate.setDefaultNightMode(Settings.theme)
     }
 }
