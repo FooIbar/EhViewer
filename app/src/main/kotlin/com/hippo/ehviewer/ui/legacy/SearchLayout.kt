941e7d18f3da680a9d35c7574a62e7eab190723e
FooIbar <118464521+FooIbar@users.noreply.github.com>
2023-11-16 09:34:29 +0800

    Implement pull to refresh in GalleryCommentsScreen (#272)
    * Use AndroidX snapshot builds
    
    * Implement pull to refresh in GalleryCommentsScreen
    
    * Don't use snapshot builds for other libraries

--- app/src/main/kotlin/com/hippo/ehviewer/ui/legacy/SearchLayout.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/ui/legacy/SearchLayout.kt
@@ -17,8 +17,8 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SecondaryTabRow
 import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
@@ -137,7 +137,7 @@ class SearchLayout @JvmOverloads constructor(
                         }
                     }
                 }
-                TabRow(
+                SecondaryTabRow(
                     selectedTabIndex = if (vm.isNormalMode) 0 else 1,
                     divider = {},
                 ) {
