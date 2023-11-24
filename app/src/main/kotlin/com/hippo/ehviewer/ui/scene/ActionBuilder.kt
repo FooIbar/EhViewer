b991e5d70a49f50b59b03fbded8bea957747ff6f
GG Bond <whatever@tagkong.com>
2023-11-24 22:04:40 +0800

    Cleanup ActionBuilder

--- app/src/main/kotlin/com/hippo/ehviewer/ui/scene/ActionBuilder.kt
+++
@@ -1,9 +0,0 @@
-package com.hippo.ehviewer.ui.scene
-
-fun interface ActionScope {
-    fun onSelect(action: String, that: suspend () -> Unit)
-}
-
-inline fun buildAction(builder: ActionScope.() -> Unit) = buildList {
-    builder { action, that -> add(action to that) }
-}
