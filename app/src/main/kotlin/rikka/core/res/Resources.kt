015d943543060263cf1e5af431a4d0a95c24b2b9
GG Bond <whatever@tagkong.com>
2023-11-27 22:13:48 +0800

    Cleanup unused

--- app/src/main/kotlin/rikka/core/res/Resources.kt
+++ app/src/main/kotlin/rikka/core/res/Resources.kt
@@ -1,7 +1,6 @@
 package rikka.core.res
 
 import android.content.res.Resources
-import android.graphics.drawable.Drawable
 import androidx.annotation.AttrRes
 import androidx.annotation.ColorInt
 
@@ -9,13 +8,6 @@ import androidx.annotation.ColorInt
 fun Resources.Theme.resolveColor(@AttrRes attrId: Int): Int {
     val a = obtainStyledAttributes(intArrayOf(attrId))
     val res = a.getColor(0, 0)
-    a.recycle()
-    return res
-}
-
-fun Resources.Theme.resolveDrawable(@AttrRes attrId: Int): Drawable? {
-    val a = obtainStyledAttributes(intArrayOf(attrId))
-    val res = a.getDrawable(0)
     a.recycle()
     return res
 }
