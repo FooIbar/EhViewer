69ac9d4d5c5da91f1d6a839bee4b1b1a5da68cfe
FooIbar <118464521+FooIbar@users.noreply.github.com>
2023-12-01 14:14:07 +0800

    Add comment

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,6 +15,7 @@ dependencyResolutionManagement {
         maven("https://jcenter.bintray.com")
         maven("https://jitpack.io")
         maven("https://androidx.dev/storage/compose-compiler/repository/")
+        // Test if IME not hiding after performing search is fixed before updating.
         maven("https://androidx.dev/snapshots/builds/11157558/artifacts/repository")
     }
 }
