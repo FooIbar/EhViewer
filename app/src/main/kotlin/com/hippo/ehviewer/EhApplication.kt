d1c7434993b184e28c1639bae304a1410b94f649
GG Bond <whatever@tagkong.com>
2023-12-04 20:06:54 +0800

    Drop CloudflareInterceptor to synchronize behaviour

--- app/src/main/kotlin/com/hippo/ehviewer/EhApplication.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/EhApplication.kt
@@ -52,7 +52,6 @@ import com.hippo.ehviewer.util.isAtLeastP
 import com.hippo.ehviewer.util.isAtLeastQ
 import com.hippo.ehviewer.util.isAtLeastS
 import com.hippo.ehviewer.util.isCronetSupported
-import eu.kanade.tachiyomi.network.interceptor.CloudflareInterceptor
 import eu.kanade.tachiyomi.network.interceptor.UncaughtExceptionInterceptor
 import eu.kanade.tachiyomi.util.lang.launchIO
 import eu.kanade.tachiyomi.util.lang.withUIContext
@@ -210,7 +209,6 @@ class EhApplication : Application(), ImageLoaderFactory {
                     dns(AsyncDns.toDns(AndroidAsyncDns.IPv4, AndroidAsyncDns.IPv6))
                 }
                 addInterceptor(UncaughtExceptionInterceptor())
-                addInterceptor(CloudflareInterceptor(appCtx))
             }
         }
 
