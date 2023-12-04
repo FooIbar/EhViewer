cc3f515d9aaf90b6ee4efdbe6b94f3ef1d73e29d
FooIbar <118464521+FooIbar@users.noreply.github.com>
2023-12-04 12:23:42 +0800

    Readd QUIC hints as cronet engine is now preferred

--- app/src/oss/kotlin/com/hippo/ehviewer/cronet/CronetKt.kt
+++ app/src/oss/kotlin/com/hippo/ehviewer/cronet/CronetKt.kt
@@ -30,6 +30,10 @@ val cronetHttpClient: HttpEngine = HttpEngine.Builder(appCtx).apply {
     val cache = (appCtx.cacheDir.toOkioPath() / "http_cache").toFile().apply { mkdirs() }
     setStoragePath(cache.absolutePath)
     setEnableHttpCache(HttpEngine.Builder.HTTP_CACHE_DISK_NO_HTTP, 4096)
+
+    addQuicHint("e-hentai.org", 443, 443)
+    addQuicHint("forums.e-hentai.org", 443, 443)
+    addQuicHint("exhentai.org", 443, 443)
 }.build()
 
 class CronetRequest {
