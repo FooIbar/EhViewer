cecd6fb359af46e9246d4a8e431a9e9ef53becc8
FooIbar <118464521+FooIbar@users.noreply.github.com>
2023-12-06 13:31:00 +0800

    [no ci] Remove deprecated JCenter repository

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven("https://jcenter.bintray.com")
         maven("https://jitpack.io")
         maven("https://androidx.dev/storage/compose-compiler/repository/")
         // Test if IME not hiding after performing search is fixed before updating.
