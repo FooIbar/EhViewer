c55d393acbb2b611c816c14448a5e4ad22455b2c
GG Bond <whatever@tagkong.com>
2023-12-27 00:45:53 +0800

    Refine

--- app/src/main/kotlin/com/hippo/unifile/RawFile.kt
+++ app/src/main/kotlin/com/hippo/unifile/RawFile.kt
@@ -100,7 +100,7 @@ class RawFile(parent: UniFile?, private var mFile: File) : UniFile(parent) {
 
     override fun findFirst(filter: (String) -> Boolean) = allChildren.firstOrNull { filter(it.name) }
 
-    override fun findFile(displayName: String) = allChildren.firstOrNull { it.name == displayName }
+    override fun findFile(displayName: String) = subFile(displayName).takeIf { it.exists() }
 
     override fun renameTo(displayName: String): Boolean {
         val target = File(mFile.parentFile, displayName)
