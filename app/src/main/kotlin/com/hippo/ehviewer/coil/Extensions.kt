b03803538a1d66f38613da0cb8ac05ec402b4f66
GG Bond <whatever@tagkong.com>
2023-12-29 11:52:57 +0800

    Move to coil NetworkFetcher

--- app/src/main/kotlin/com/hippo/ehviewer/coil/Extensions.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/coil/Extensions.kt
@@ -7,18 +7,31 @@ import coil3.decode.DecodeResult
 import coil3.decode.Decoder
 import coil3.request.CachePolicy
 import coil3.request.ImageRequest
+import coil3.request.httpHeaders
 import coil3.size.Size
+import com.hippo.ehviewer.Settings
+import com.hippo.ehviewer.client.CHROME_ACCEPT
+import com.hippo.ehviewer.client.CHROME_ACCEPT_LANGUAGE
 import com.hippo.ehviewer.client.data.GalleryInfo
 import com.hippo.ehviewer.client.data.GalleryPreview
 import com.hippo.ehviewer.client.data.NormalGalleryPreview
 import com.hippo.ehviewer.client.url
 import com.hippo.ehviewer.spider.DownloadInfoMagics.encodeMagicRequestOrUrl
+import io.ktor.http.HttpHeaders
+import io.ktor.http.headers
+
+private val header = headers {
+    append(HttpHeaders.UserAgent, Settings.userAgent)
+    append(HttpHeaders.Accept, CHROME_ACCEPT)
+    append(HttpHeaders.AcceptLanguage, CHROME_ACCEPT_LANGUAGE)
+}
 
 fun ImageRequest.Builder.ehUrl(info: GalleryInfo) = apply {
     val key = info.thumbKey!!
     data(encodeMagicRequestOrUrl(info))
     memoryCacheKey(key)
     diskCacheKey(key)
+    httpHeaders(header)
 }
 
 fun ImageRequest.Builder.ehPreview(preview: GalleryPreview) = apply {
@@ -26,6 +39,7 @@ fun ImageRequest.Builder.ehPreview(preview: GalleryPreview) = apply {
     memoryCacheKey(preview.imageKey)
     diskCacheKey(preview.imageKey)
     if (preview is NormalGalleryPreview) size(Size.ORIGINAL)
+    httpHeaders(header)
 }
 
 private val stubImage = ColorDrawable(Color.BLACK).asCoilImage(true)
