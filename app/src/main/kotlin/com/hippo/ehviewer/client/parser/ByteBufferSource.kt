080f1631e0e221865500e60d8fdfec9e6d4a0ed7
GG Bond <whatever@tagkong.com>
2023-12-31 15:05:07 +0800

    Use Cbor

--- app/src/main/kotlin/com/hippo/ehviewer/client/parser/ByteBufferSource.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/client/parser/ByteBufferSource.kt
@@ -1,34 +1,13 @@
 package com.hippo.ehviewer.client.parser
 
-import com.hippo.ehviewer.client.parseAs
 import eu.kanade.tachiyomi.util.system.logcat
+import io.ktor.util.moveToByteArray
 import java.nio.ByteBuffer
 import kotlin.system.measureNanoTime
-import okio.Buffer
-import okio.Source
-import okio.Timeout
-import okio.buffer
-
-fun ByteBuffer.asSource() = object : Source {
-    private val buffer = this@asSource.slice()
-    private val len = buffer.capacity()
-
-    override fun close() = Unit
-
-    override fun read(sink: Buffer, byteCount: Long): Long {
-        if (buffer.position() == len) return -1
-        val pos = buffer.position()
-        val newLimit = (pos + byteCount).toInt().coerceAtMost(len)
-        buffer.limit(newLimit)
-        return sink.write(buffer).toLong()
-    }
-
-    override fun timeout() = Timeout.NONE
-}
+import kotlinx.serialization.cbor.Cbor
+import kotlinx.serialization.decodeFromByteArray
 
 inline fun <reified T> unmarshalParsingAs(body: ByteBuffer, parser: (ByteBuffer, Int) -> Int): T {
-    // We want to use cbor as it have smaller output size
-    // But we would rather reuse allocated ByteArrays (i.e. decode with okio Buffer)
     val jsonBytes: Int
     measureNanoTime {
         jsonBytes = parser(body, body.limit())
@@ -37,7 +16,8 @@ inline fun <reified T> unmarshalParsingAs(body: ByteBuffer, parser: (ByteBuffer,
     body.limit(jsonBytes)
     val t: T
     measureNanoTime {
-        t = body.asSource().buffer().use { it.parseAs<T>() }
+        val array = body.slice().moveToByteArray()
+        t = Cbor.decodeFromByteArray(array)
     }.also { it.logcat { "Unmarshal + allocate java use $it ns" } }
     return t
 }
