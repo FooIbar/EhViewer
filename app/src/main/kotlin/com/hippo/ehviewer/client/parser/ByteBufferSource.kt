73643d35920b89760960027a7f6aa2f54f8c68aa
GG Bond <whatever@tagkong.com>
2023-12-31 15:20:57 +0800

    Drop logging

--- app/src/main/kotlin/com/hippo/ehviewer/client/parser/ByteBufferSource.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/client/parser/ByteBufferSource.kt
@@ -1,22 +1,13 @@
 package com.hippo.ehviewer.client.parser
 
-import eu.kanade.tachiyomi.util.system.logcat
 import io.ktor.util.moveToByteArray
 import java.nio.ByteBuffer
-import kotlin.system.measureNanoTime
 import kotlinx.serialization.cbor.Cbor
 import kotlinx.serialization.decodeFromByteArray
 
 inline fun <reified T> unmarshalParsingAs(body: ByteBuffer, parser: (ByteBuffer, Int) -> Int): T {
-    val jsonBytes: Int
-    measureNanoTime {
-        jsonBytes = parser(body, body.limit())
-    }.also { it.logcat { "Parse + marshal use $it ns" } }
-    body.limit(jsonBytes)
-    val t: T
-    measureNanoTime {
-        val array = body.moveToByteArray()
-        t = Cbor.decodeFromByteArray(array)
-    }.also { it.logcat { "Unmarshal + allocate java use $it ns" } }
-    return t
+    val cborBytes = parser(body, body.limit())
+    body.limit(cborBytes)
+    val array = body.moveToByteArray()
+    return Cbor.decodeFromByteArray<T>(array)
 }
