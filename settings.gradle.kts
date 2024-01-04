515f7deb7ea305afe3dc2f6be98d07990a89259b
Vladimir Dostoyevskiy <99822064+VDostoyevskiy@users.noreply.github.com>
2024-01-04 08:59:57 +0800

    Update compose snapshot

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,7 +15,7 @@ dependencyResolutionManagement {
         maven("https://oss.sonatype.org/content/repositories/snapshots")
         maven("https://jitpack.io")
         maven("https://androidx.dev/storage/compose-compiler/repository/")
-        maven("https://androidx.dev/snapshots/builds/11260879/artifacts/repository")
+        maven("https://androidx.dev/snapshots/builds/11271125/artifacts/repository")
     }
 }
 
