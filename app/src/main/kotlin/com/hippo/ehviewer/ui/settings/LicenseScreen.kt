c3ffab3c79204931661819ff45ded61e80908bb9
GG Bond <whatever@tagkong.com>
2024-01-07 10:36:33 +0800

    Move to official aboutlibraries m3 widget

--- app/src/main/kotlin/com/hippo/ehviewer/ui/settings/LicenseScreen.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/ui/settings/LicenseScreen.kt
@@ -1,9 +1,11 @@
 package com.hippo.ehviewer.ui.settings
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -11,9 +13,13 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.hippo.ehviewer.R
-import com.mikepenz.aboutlibraries.ui.compose.LibrariesContainer
+import com.hippo.ehviewer.ui.openBrowser
+import com.mikepenz.aboutlibraries.ui.compose.m3.LibrariesContainer
+import com.mikepenz.aboutlibraries.ui.compose.m3.LibraryDefaults
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
@@ -21,6 +27,7 @@ import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 @Composable
 fun LicenseScreen(navigator: DestinationsNavigator) {
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+    val context = LocalContext.current
     Scaffold(
         topBar = {
             TopAppBar(
@@ -33,7 +40,15 @@ fun LicenseScreen(navigator: DestinationsNavigator) {
                 scrollBehavior = scrollBehavior,
             )
         },
-    ) {
-        LibrariesContainer(modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection), contentPadding = it)
+    ) { paddingValues ->
+        LibrariesContainer(
+            modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+            contentPadding = paddingValues,
+            colors = LibraryDefaults.libraryColors(badgeBackgroundColor = MaterialTheme.colorScheme.tertiary),
+            padding = LibraryDefaults.libraryPadding(badgeContentPadding = PaddingValues(4.dp)),
+            onLibraryClick = { library ->
+                library.library.website?.let { context.openBrowser(it) }
+            },
+        )
     }
 }
