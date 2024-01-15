3c5375a9a61035a6138d7a4bc01eadc8d0467fe8
FooIbar <118464521+FooIbar@users.noreply.github.com>
2024-01-15 23:41:08 +0800

    Prepare for next iteration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,7 +65,8 @@ android {
         minSdk = 28
         targetSdk = 34
         versionCode = 180050
-        versionName = "1.10.3"
+        versionName = "1.11.0"
+        versionNameSuffix = "-SNAPSHOT"
         resourceConfigurations.addAll(
             listOf(
                 "zh",
