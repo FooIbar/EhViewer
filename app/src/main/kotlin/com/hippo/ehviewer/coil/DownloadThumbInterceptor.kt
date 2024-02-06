e1ad4e3b9055fa8e3516d52f5056f6bb24a06688
FooIbar <118464521+FooIbar@users.noreply.github.com>
2024-02-06 22:52:45 +0800

    Rename .thumb to thumb.jpg so it can be previewed

--- app/src/main/kotlin/com/hippo/ehviewer/coil/DownloadThumbInterceptor.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/coil/DownloadThumbInterceptor.kt
@@ -12,12 +12,22 @@ import com.hippo.unifile.asUniFile
 import eu.kanade.tachiyomi.util.lang.withIOContext
 
 object DownloadThumbInterceptor : Interceptor {
+    // TODO: Remove this after a few releases
+    private const val LEGACY_THUMB_FILE = ".thumb"
+    private const val THUMB_FILE = "thumb.jpg"
     override suspend fun intercept(chain: Interceptor.Chain): ImageResult {
         val magicOrUrl = chain.request.data as? String
         if (magicOrUrl != null) {
             val (url, location) = decodeMagicRequestOrUrl(magicOrUrl)
             if (location != null) {
-                val thumb = withIOContext { downloadLocation / location / ".thumb" }
+                val thumb = withIOContext {
+                    val legacyThumb = downloadLocation / location / LEGACY_THUMB_FILE
+                    if (legacyThumb.isFile) {
+                        legacyThumb.apply { renameTo(THUMB_FILE) }
+                    } else {
+                        downloadLocation / location / THUMB_FILE
+                    }
+                }
                 if (withIOContext { thumb.isFile }) {
                     val new = chain.request.newBuilder().data(thumb.uri.toString()).build()
                     when (val result = chain.withRequest(new).proceed()) {
