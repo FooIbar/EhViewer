f844a42d15676dc80c55f54444997f1f0ddae5f6
GG Bond <whatever@tagkong.com>
2024-02-10 21:40:24 +0800

    Update compose snapshot

--- app/src/main/kotlin/com/hippo/ehviewer/ui/screen/DownloadsScreen.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/ui/screen/DownloadsScreen.kt
@@ -20,7 +20,7 @@ import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.text2.input.rememberTextFieldState
+import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.DriveFileMove
 import androidx.compose.material.icons.automirrored.outlined.Label
