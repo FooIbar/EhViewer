14810b4c2c385e302d30ffa049122382ba1622a8
GG Bond <whatever@tagkong.com>
2024-02-16 12:02:40 +0800

    Grab some RestrictedApi to make lint happy

--- app/src/main/kotlin/com/hippo/ehviewer/ui/tools/PagingUtils.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/ui/tools/PagingUtils.kt
@@ -1,6 +1,11 @@
 package com.hippo.ehviewer.ui.tools
 
+import androidx.paging.PagingSource.LoadParams
+import androidx.paging.PagingSource.LoadParams.Append
+import androidx.paging.PagingSource.LoadParams.Prepend
+import androidx.paging.PagingSource.LoadParams.Refresh
 import androidx.paging.PagingSource.LoadResult
+import androidx.paging.PagingState
 
 inline fun <K : Any, V : Any, R : LoadResult.Page<K, V>, T> Result<T>.foldToLoadResult(
     onSuccess: (value: T) -> R,
@@ -8,3 +13,19 @@ inline fun <K : Any, V : Any, R : LoadResult.Page<K, V>, T> Result<T>.foldToLoad
     onSuccess = onSuccess,
     onFailure = { LoadResult.Error(it) },
 )
+
+fun <Value : Any> PagingState<Int, Value>.getClippedRefreshKey() = when (val anchorPosition = anchorPosition) {
+    null -> null
+    else -> maxOf(0, anchorPosition - (config.initialLoadSize / 2))
+}
+
+fun getOffset(params: LoadParams<Int>, key: Int, itemCount: Int) = when (params) {
+    is Prepend -> if (key < params.loadSize) 0 else key - params.loadSize
+    is Append -> key
+    is Refresh -> if (key >= itemCount) maxOf(0, itemCount - params.loadSize) else key
+}
+
+fun getLimit(params: LoadParams<Int>, key: Int) = when (params) {
+    is Prepend -> if (key < params.loadSize) key else params.loadSize
+    else -> params.loadSize
+}
