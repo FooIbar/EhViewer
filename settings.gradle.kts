ab5af22fc380d5f6fdebdc45a44b046b318ac052
FooIbar <118464521+FooIbar@users.noreply.github.com>
2024-02-17 12:21:11 +0800

    Re-enable gradle configuration cache

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,6 +18,6 @@ dependencyResolutionManagement {
 }
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-// enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
+enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
 rootProject.name = "EhViewer"
 include(":app")
