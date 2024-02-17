7d2e0f7f7ed228fc13f216551d29353a5ae2b44f
FooIbar <118464521+FooIbar@users.noreply.github.com>
2024-02-17 19:05:12 +0800

    Build with Android 15 preview SDK (#749)
    * Build with Android 15 preview SDK
    
    * Fix CI
    
    * Don't set application as test-only

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,13 +8,3 @@ plugins {
     alias(libs.plugins.aboutlibrariesPlugin) apply false
     alias(libs.plugins.composeCompilerReportGenerator) apply false
 }
-
-tasks.register("Delete", Delete::class) {
-    delete(rootProject.layout.buildDirectory)
-}
-
-buildscript {
-    dependencies {
-        classpath(libs.r8)
-    }
-}
