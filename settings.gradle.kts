bc0a2a66db88838727fe63780985834eb7b5e25e
GG Bond <whatever@tagkong.com>
2024-02-23 10:43:57 +0800

    Update snapshot

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,7 +13,7 @@ dependencyResolutionManagement {
         mavenCentral()
         maven("https://jitpack.io")
         maven("https://androidx.dev/storage/compose-compiler/repository/")
-        maven("https://androidx.dev/snapshots/builds/11476622/artifacts/repository")
+        maven("https://androidx.dev/snapshots/builds/11487711/artifacts/repository")
     }
 }
 
