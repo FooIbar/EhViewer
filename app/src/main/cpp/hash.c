5f3af806f69c3fb22682e9043ba79ac008e521b8
FooIbar <118464521+FooIbar@users.noreply.github.com>
2024-02-25 00:05:32 +0800

    Inline `bytes_to_hex` and allocate buffer on stack

--- app/src/main/cpp/hash.c
+++ app/src/main/cpp/hash.c
@@ -1,15 +1,9 @@
-#include <errno.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 
-#include <android/log.h>
 #include <jni.h>
 #include <sha1.h>
 
 #include "ehviewer.h"
-
-#define LOG_TAG "hash"
 
 #define BUFFER_SIZE 8192
 
@@ -17,31 +11,14 @@ typedef uint8_t byte;
 
 const char hex_digits[] = "0123456789abcdef";
 
-char *bytes_to_hex(const byte *bytes, size_t length) {
-    byte byte;
-    char *hex = malloc(2 * length + 1);
-    for (int i = 0; i < length; i++) {
-        byte = bytes[i];
-        hex[2 * i] = hex_digits[byte >> 4 & 0xF];
-        hex[2 * i + 1] = hex_digits[byte & 0xF];
-    }
-    hex[2 * length] = '\0';
-    return hex;
-}
-
 JNIEXPORT jstring JNICALL
 Java_com_hippo_ehviewer_jni_HashKt_sha1(JNIEnv *env, jclass clazz, jint fd) {
     EH_UNUSED(clazz);
-    byte *buffer = malloc(BUFFER_SIZE);
-    if (!buffer) {
-        LOGE("%s", strerror(errno));
-        return NULL;
-    }
-
     struct sha1_ctx ctx;
     sha1_init(&ctx);
 
     size_t bytes_read;
+    byte buffer[BUFFER_SIZE];
     while ((bytes_read = read(fd, buffer, BUFFER_SIZE)) > 0) {
         sha1_update(&ctx, bytes_read, buffer);
     }
@@ -49,9 +26,14 @@ Java_com_hippo_ehviewer_jni_HashKt_sha1(JNIEnv *env, jclass clazz, jint fd) {
     byte digest[SHA1_DIGEST_SIZE];
     sha1_digest(&ctx, SHA1_DIGEST_SIZE, digest);
 
-    char *hex_digest = bytes_to_hex(digest, SHA1_DIGEST_SIZE);
-    jstring str = (*env)->NewStringUTF(env, hex_digest);
-    free(buffer);
-    free(hex_digest);
-    return str;
+    byte byte;
+    char hex_digest[2 * SHA1_DIGEST_SIZE + 1];
+    for (int i = 0; i < SHA1_DIGEST_SIZE; i++) {
+        byte = digest[i];
+        hex_digest[2 * i] = hex_digits[byte >> 4 & 0xF];
+        hex_digest[2 * i + 1] = hex_digits[byte & 0xF];
+    }
+    hex_digest[2 * SHA1_DIGEST_SIZE] = '\0';
+
+    return (*env)->NewStringUTF(env, hex_digest);
 }
--- app/src/main/kotlin/com/hippo/ehviewer/jni/Hash.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/jni/Hash.kt
@@ -1,3 +1,3 @@
 package com.hippo.ehviewer.jni
 
-external fun sha1(fd: Int): String?
+external fun sha1(fd: Int): String
