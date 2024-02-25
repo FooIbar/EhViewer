ce94b948e023bae1d70d10f12d9b565d1573ea9c
GG Bond <whatever@tagkong.com>
2024-02-25 22:56:37 +0800

    Fuck off silly error handling
    Signed-off-by: GG Bond <whatever@tagkong.com>

--- app/src/main/rust/src/hash/sha1.rs
+++ app/src/main/rust/src/hash/sha1.rs
@@ -1,12 +1,12 @@
-use anyhow::Result;
+use anyhow::{Ok, Result};
 use base16ct::lower::encode_str;
 use jni::objects::JClass;
 use jni::sys::{jint, jstring};
 use jni::JNIEnv;
 use jni_fn::jni_fn;
+use jni_throwing;
 use sha1::{Digest, Sha1};
 use std::{fs::File, io::copy, os::fd::BorrowedFd};
-use throw_msg;
 
 pub fn file_sha1(mut file: File, digest: &mut [u8; 40]) -> Result<&str> {
     let mut sha = Sha1::new();
@@ -18,18 +18,11 @@ pub fn file_sha1(mut file: File, digest: &mut [u8; 40]) -> Result<&str> {
 #[allow(non_snake_case)]
 #[jni_fn("com.hippo.ehviewer.jni.HashKt")]
 pub fn sha1(mut env: JNIEnv, _class: JClass, fd: jint) -> jstring {
-    let borrowed_fd = unsafe { BorrowedFd::borrow_raw(fd) };
-    let owned_fd = match borrowed_fd.try_clone_to_owned() {
-        Ok(fd) => fd,
-        Err(err) => return throw_msg(&mut env, &format!("{err}")) as jstring,
-    };
-    let mut hex_digest = [0u8; 40];
-    let hex = match file_sha1(File::from(owned_fd), &mut hex_digest) {
-        Ok(str) => str,
-        Err(err) => return throw_msg(&mut env, &format!("{err}")) as jstring,
-    };
-    match env.new_string(hex) {
-        Ok(obj) => obj.into_raw(),
-        Err(err) => throw_msg(&mut env, &format!("{err}")) as jstring,
-    }
+    jni_throwing(&mut env, |env| {
+        let borrowed_fd = unsafe { BorrowedFd::borrow_raw(fd) };
+        let owned_fd = borrowed_fd.try_clone_to_owned()?;
+        let mut hex_digest = [0u8; 40];
+        let hex = file_sha1(File::from(owned_fd), &mut hex_digest)?;
+        Ok(env.new_string(hex)?.into_raw())
+    })
 }
