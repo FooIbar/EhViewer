7cbc641178478a3e29ca4f995573eae3b310b721
GG Bond <whatever@tagkong.com>
2024-02-27 13:49:27 +0800

    Drop outdated comment

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,7 +13,6 @@ dependencyResolutionManagement {
         mavenCentral()
         maven("https://jitpack.io")
         maven("https://androidx.dev/storage/compose-compiler/repository/")
-        // Hold until https://issuetracker.google.com/issues/325372937#comment4 is fixed
         maven("https://androidx.dev/snapshots/builds/11500003/artifacts/repository")
     }
 }
