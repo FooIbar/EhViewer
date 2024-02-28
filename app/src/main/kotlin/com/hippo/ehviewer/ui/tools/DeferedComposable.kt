beeb4bdbd45712d37b84cc10d09f0f72c5648ace
GG Bond <whatever@tagkong.com>
2024-02-28 15:01:01 +0800

    Crop border for Compose ReaderScreen

--- app/src/main/kotlin/com/hippo/ehviewer/ui/tools/DeferedComposable.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/ui/tools/DeferedComposable.kt
@@ -6,13 +6,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import eu.kanade.tachiyomi.util.lang.withIOContext
 
 @Composable
 fun <T> Deferred(block: suspend () -> T, content: @Composable (T) -> Unit) {
-    var completed by remember { mutableStateOf<T?>(null) }
-    LaunchedEffect(key1 = Unit) {
-        completed = withIOContext { block() }
+    var completed by remember(block) { mutableStateOf<T?>(null) }
+    LaunchedEffect(block) {
+        completed = block()
     }
     completed?.let { content(it) }
 }
