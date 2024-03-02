886e025ac81d4c0853f8559a8e77255e867a2b3d
GG Bond <whatever@tagkong.com>
2024-03-02 20:37:38 +0800

    Implement copy

--- app/src/main/kotlin/com/hippo/ehviewer/ui/reader/PageActions.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/ui/reader/PageActions.kt
@@ -1,5 +1,6 @@
 package com.hippo.ehviewer.ui.reader
 
+import android.content.ClipData
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
@@ -13,9 +14,11 @@ import com.hippo.ehviewer.client.data.GalleryInfo
 import com.hippo.ehviewer.gallery.PageLoader2
 import com.hippo.ehviewer.util.AppConfig
 import com.hippo.ehviewer.util.FileUtils
+import com.hippo.ehviewer.util.isAtLeastT
 import com.hippo.unifile.asUniFile
 import eu.kanade.tachiyomi.ui.reader.model.ReaderPage
 import java.io.File
+import splitties.systemservices.clipboardManager
 
 context(PageLoader2)
 private fun Context.provideImage(index: Int): Uri? {
@@ -46,5 +49,21 @@ suspend fun shareImage(page: ReaderPage, info: GalleryInfo? = null) {
         startActivity(Intent.createChooser(intent, share))
     } catch (e: Throwable) {
         showSnackbar(noActivity)
+    }
+}
+
+context(SnackbarHostState, Context, PageLoader2)
+suspend fun copy(page: ReaderPage) {
+    val error = getString(R.string.error_cant_save_image)
+    val copied = getString(R.string.copied_to_clipboard)
+    val uri = provideImage(page.index)
+    if (uri == null) {
+        showSnackbar(error)
+        return
+    }
+    val clipData = ClipData.newUri(contentResolver, "ehviewer", uri)
+    clipboardManager.setPrimaryClip(clipData)
+    if (!isAtLeastT) {
+        showSnackbar(copied)
     }
 }
