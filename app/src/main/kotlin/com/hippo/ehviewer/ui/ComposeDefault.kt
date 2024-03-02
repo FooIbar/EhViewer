be4541c5d38eb2a08f7abac3e6f7a0674dfae659
GG Bond <whatever@tagkong.com>
2024-03-02 21:47:51 +0800

    composing with MainActivity

--- app/src/main/kotlin/com/hippo/ehviewer/ui/ComposeDefault.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/ui/ComposeDefault.kt
@@ -1,18 +1,25 @@
 package com.hippo.ehviewer.ui
 
-import android.content.Context
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.platform.LocalContext
 import com.hippo.ehviewer.ui.tools.DialogState
 import com.hippo.ehviewer.ui.tools.LocalDialogState
+import com.hippo.ehviewer.util.findActivity
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import kotlinx.coroutines.CoroutineScope
 
 @Composable
 inline fun <R> composing(
     navigator: DestinationsNavigator,
-    block: @Composable context(Context, SnackbarHostState, DialogState, DestinationsNavigator, CoroutineScope)
+    block: @Composable context(MainActivity, SnackbarHostState, DialogState, DestinationsNavigator, CoroutineScope)
     () -> R,
-) = block(LocalContext.current, LocalSnackBarHostState.current, LocalDialogState.current, navigator, rememberCoroutineScope())
+) = block(
+    LocalContext.current.run { remember { findActivity<MainActivity>() } },
+    LocalSnackBarHostState.current,
+    LocalDialogState.current,
+    navigator,
+    rememberCoroutineScope(),
+)
