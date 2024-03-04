FooIbar/EhViewer

commit 4142f28822772a9eb145c3532075812479fd5829

Fix simple language generation for favorites

app/src/main/kotlin/com/hippo/ehviewer/client/EhEngine.kt
@@ -20,6 +20,7 @@ import arrow.core.left
 import arrow.core.right
 import arrow.fx.coroutines.parMap
 import arrow.fx.coroutines.parZip
+import com.hippo.ehviewer.EhDB
 import com.hippo.ehviewer.R
 import com.hippo.ehviewer.Settings
 import com.hippo.ehviewer.client.data.BaseGalleryInfo
@@ -436,6 +437,12 @@ object EhEngine {
         val needApi = filter && needTags() && !hasTags || Settings.showGalleryPages.value && !hasPages || hasRated
         if (needApi) fillGalleryListByApi(this@fillInfo, url)
         if (filter) removeAllSuspend { filterUploader(it) || filterTag(it) || filterTagNamespace(it) }
+        forEach {
+            if (it.favoriteSlot == GalleryInfo.NOT_FAVORITED && EhDB.containLocalFavorites(it.gid)) {
+                it.favoriteSlot = GalleryInfo.LOCAL_FAVORITED
+            }
+            it.generateSLang()
+        }
     }
 
     suspend fun addFavorites(galleryList: List<Pair<Long, String>>, dstCat: Int) {

app/src/main/kotlin/com/hippo/ehviewer/client/parser/GalleryApiParser.kt
@@ -38,7 +38,6 @@ object GalleryApiParser {
                 rating = item.rating
                 simpleTags = item.tags
                 pages = item.pages
-                generateSLang()
             }
         }
     }

app/src/main/kotlin/com/hippo/ehviewer/client/parser/GalleryListParser.kt
@@ -1,26 +1,16 @@
 package com.hippo.ehviewer.client.parser
 
-import com.hippo.ehviewer.EhDB
 import com.hippo.ehviewer.client.data.BaseGalleryInfo
-import com.hippo.ehviewer.client.data.GalleryInfo.Companion.LOCAL_FAVORITED
-import com.hippo.ehviewer.client.data.GalleryInfo.Companion.NOT_FAVORITED
 import com.hippo.ehviewer.client.exception.ParseException
 import java.nio.ByteBuffer
 import kotlinx.serialization.Serializable
 import moe.tarsin.coroutines.runSuspendCatching
 
 object GalleryListParser {
-    suspend fun parse(body: ByteBuffer) = runSuspendCatching {
+    fun parse(body: ByteBuffer) = runSuspendCatching {
         unmarshalParsingAs<GalleryListResult>(body, ::parseGalleryInfoList)
     }.getOrElse {
         throw ParseException("Can't parse gallery list", it)
-    }.apply {
-        galleryInfoList.onEach {
-            if (it.favoriteSlot == NOT_FAVORITED && EhDB.containLocalFavorites(it.gid)) {
-                it.favoriteSlot = LOCAL_FAVORITED
-            }
-            it.generateSLang()
-        }
     }
 
     val emptyResult = GalleryListResult(null, null, arrayListOf())

commit 1e4d5504127dbce83e6d2a88e44874e2a48eeec2

Fix simple language generation for ComicInfo
Regression introduced in commit 4142f288

app/src/main/kotlin/com/hippo/ehviewer/client/EhEngine.kt
@@ -441,7 +441,7 @@ object EhEngine {
             if (it.favoriteSlot == GalleryInfo.NOT_FAVORITED && EhDB.containLocalFavorites(it.gid)) {
                 it.favoriteSlot = GalleryInfo.LOCAL_FAVORITED
             }
-            it.generateSLang()
+            if (!needApi) it.generateSLang()
         }
     }
 

app/src/main/kotlin/com/hippo/ehviewer/client/parser/GalleryApiParser.kt
@@ -38,6 +38,7 @@ object GalleryApiParser {
                 rating = item.rating
                 simpleTags = item.tags
                 pages = item.pages
+                generateSLang()
             }
         }
     }