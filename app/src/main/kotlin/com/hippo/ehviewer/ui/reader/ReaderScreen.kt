e0f6364938e8d69f3fddbaebb77557cc08029c40
GG Bond <whatever@tagkong.com>
2024-03-05 17:43:17 +0800

    Apply bgColor

--- app/src/main/kotlin/com/hippo/ehviewer/ui/reader/ReaderScreen.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/ui/reader/ReaderScreen.kt
@@ -1,5 +1,7 @@
 package com.hippo.ehviewer.ui.reader
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
@@ -15,14 +17,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.util.VelocityTrackerAddPointsFix
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.zIndex
+import com.hippo.ehviewer.R
 import com.hippo.ehviewer.Settings
 import com.hippo.ehviewer.client.data.BaseGalleryInfo
 import com.hippo.ehviewer.collectAsState
@@ -113,6 +119,7 @@ fun ReaderScreen(info: BaseGalleryInfo, page: Int = -1, navigator: DestinationsN
                     modifier = Modifier.align(Alignment.BottomCenter).navigationBarsPadding().zIndex(0.5f),
                 )
             }
+            val bgColor by collectBackgroundColorAsState()
             GalleryPager(
                 type = readingMode,
                 pagerState = pagerState,
@@ -143,7 +150,22 @@ fun ReaderScreen(info: BaseGalleryInfo, page: Int = -1, navigator: DestinationsN
                     }
                 },
                 onMenuRegionClick = { appbarVisible = !appbarVisible },
+                modifier = Modifier.background(bgColor),
             )
+        }
+    }
+}
+
+@Composable
+private fun collectBackgroundColorAsState(): State<Color> {
+    val grey = colorResource(R.color.reader_background_dark)
+    val dark = isSystemInDarkTheme()
+    return Settings.readerTheme.collectAsState { theme ->
+        when (theme) {
+            0 -> Color.White
+            2 -> grey
+            3 -> if (dark) grey else Color.White
+            else -> Color.Black
         }
     }
 }
