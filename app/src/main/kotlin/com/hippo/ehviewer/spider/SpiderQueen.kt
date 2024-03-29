f892398bb0425533710d4afc5f90f5c2f30eceee
FooIbar <118464521+FooIbar@users.noreply.github.com>
2024-03-29 17:14:54 +0800

    Fix `SpiderQueen` not respecting FIFO order
    And optimize preloading strategy.

--- app/src/main/kotlin/com/hippo/ehviewer/spider/SpiderQueen.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/spider/SpiderQueen.kt
@@ -547,13 +547,20 @@ class SpiderQueen private constructor(val galleryInfo: GalleryInfo) : CoroutineS
 
         private fun doLaunchDownloadJob(index: Int, force: Boolean, orgImg: Boolean = false) {
             val state = mPageStateArray[index]
-            if (!force && state == STATE_FINISHED) return
+            if (!force) {
+                if (state == STATE_FINISHED) return
+                if (index in mSpiderDen) return updatePageState(index, STATE_FINISHED)
+            }
             val currentJob = mFetcherJobMap[index]
             val skipHath = force && !orgImg && currentJob?.isActive == true
             if (force) currentJob?.cancel(CancellationException(FORCE_RETRY))
             if (currentJob?.isActive != true) {
                 mFetcherJobMap[index] = launch {
                     runCatching {
+                        delayLock.withLock {
+                            delay(mDownloadDelay - lastRequestTime.elapsedNow())
+                            lastRequestTime = TimeSource.Monotonic.markNow()
+                        }
                         mSemaphore.withPermit {
                             doInJob(index, force, orgImg, skipHath)
                         }
@@ -591,29 +598,19 @@ class SpiderQueen private constructor(val galleryInfo: GalleryInfo) : CoroutineS
                     ?: getPTokenFromMultiPageViewer(index)
             }
             updatePageState(index, STATE_DOWNLOADING)
-            if (!force && index in mSpiderDen) {
-                return updatePageState(index, STATE_FINISHED)
-            }
-            if (force) {
-                pTokenLock.withLock {
-                    val pToken = mSpiderInfo.pTokenMap[index]
-                    if (pToken == TOKEN_FAILED) mSpiderInfo.pTokenMap.remove(index)
-                }
-            }
             val previousPToken: String?
             val pToken: String
-
             pTokenLock.withLock {
+                if (force) {
+                    if (mSpiderInfo.pTokenMap[index] == TOKEN_FAILED) {
+                        mSpiderInfo.pTokenMap.remove(index)
+                    }
+                }
                 pToken = getPToken(index)
                     ?: return updatePageState(index, STATE_FAILED, PTOKEN_FAILED_MESSAGE).also {
                         mSpiderInfo.pTokenMap[index] = TOKEN_FAILED
                     }
                 previousPToken = getPToken(index - 1)
-            }
-
-            delayLock.withLock {
-                delay(mDownloadDelay - lastRequestTime.elapsedNow())
-                lastRequestTime = TimeSource.Monotonic.markNow()
             }
 
             var skipHathKey: String? = null
