df4b99637215711c47be78e82efc105bd7ef5edd
Dasoops <DasoopsNicole@gmail.com>
2024-04-16 00:36:49 +0800

    Remove unused

--- app/src/main/kotlin/com/hippo/ehviewer/dao/DownloadArtistDao.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/dao/DownloadArtistDao.kt
@@ -1,24 +1,14 @@
 package com.hippo.ehviewer.dao
 
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 
 @Dao
 interface DownloadArtistDao {
-    @Query("SELECT * FROM DOWNLOAD_ARTISTS")
-    suspend fun list(): List<DownloadArtist>
-
-    @Insert
-    suspend fun insert(downloadArtist: DownloadArtist): Long
-
     @Insert
     suspend fun insert(downloadArtists: List<DownloadArtist>)
 
     @Query("DELETE FROM DOWNLOAD_ARTISTS WHERE GID = :gid")
     suspend fun deleteByGid(gid: Long)
-
-    @Delete
-    suspend fun delete(downloadArtist: DownloadArtist)
 }
