78a4700ab5e93abb3e3098039a44169c5e1d3aab
GG Bond <whatever@tagkong.com>
2024-04-24 23:32:22 +0800

    Refine

--- app/src/main/kotlin/com/hippo/ehviewer/ui/screen/Context.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/ui/screen/Context.kt
@@ -1,3 +1,4 @@
 package com.hippo.ehviewer.ui.screen
 
+// https://github.com/Kotlin/KEEP/blob/context-parameters/proposals/context-parameters.md#standard-library-support
 inline fun <reified T> T.implicit(): T = this
