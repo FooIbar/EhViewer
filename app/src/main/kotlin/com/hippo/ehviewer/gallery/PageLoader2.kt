f3e3a7fff553cdf79069bf5ad63b6a5fd046505c
FooIbar <118464521+FooIbar@users.noreply.github.com>
2024-05-03 15:08:34 +0800

    Copy invalid filename handling logic from AOSP

--- app/src/main/kotlin/com/hippo/ehviewer/gallery/PageLoader2.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/gallery/PageLoader2.kt
@@ -17,6 +17,7 @@ package com.hippo.ehviewer.gallery
 
 import androidx.annotation.CallSuper
 import com.hippo.ehviewer.EhDB
+import com.hippo.ehviewer.util.FileUtils
 import com.hippo.unifile.UniFile
 import eu.kanade.tachiyomi.ui.reader.loader.PageLoader
 import kotlinx.coroutines.CoroutineScope
@@ -62,7 +63,7 @@ abstract class PageLoader2(private val gid: Long, var startPage: Int) : PageLoad
     protected abstract fun getImageExtension(index: Int): String
 
     fun getImageFilename(index: Int): String {
-        return "$title - ${index + 1}.${getImageExtension(index).lowercase()}"
+        return FileUtils.sanitizeFilename("$title - ${index + 1}.${getImageExtension(index).lowercase()}")
     }
 
     abstract fun save(index: Int, file: UniFile): Boolean
