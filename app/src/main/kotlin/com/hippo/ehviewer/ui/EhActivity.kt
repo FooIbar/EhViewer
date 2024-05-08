96d4193d2d80461a5a0885a7e7cad642d4bdcac5
FooIbar <118464521+FooIbar@users.noreply.github.com>
2024-05-08 23:10:14 +0800

    Fix system bars insets in fullscreen on API < 30

--- app/src/main/kotlin/com/hippo/ehviewer/ui/EhActivity.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/ui/EhActivity.kt
@@ -16,16 +16,26 @@
 package com.hippo.ehviewer.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.ActivityNavigator
 import com.hippo.ehviewer.Settings
+import com.hippo.ehviewer.util.isAtLeastR
 import eu.kanade.tachiyomi.util.view.setSecureScreen
 
 abstract class EhActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        // https://issuetracker.google.com/204791558
+        // Fix system bars insets still exist in fullscreen mode on API < 30
+        @Suppress("DEPRECATION")
+        if (!isAtLeastR) {
+            with(window.decorView) {
+                systemUiVisibility = systemUiVisibility and View.SYSTEM_UI_FLAG_LAYOUT_STABLE.inv()
+            }
+        }
     }
 
     override fun onResume() {
