57313ebfb5baacb07973babb394c9eb864dc2aa3
FooIbar <118464521+FooIbar@users.noreply.github.com>
2024-05-23 16:22:04 +0800

    Update Compose snapshot
    https://issuetracker.google.com/333584635

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,8 +12,8 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven("https://jitpack.io")
-        // TODO: Remove on Compose 1.7.0-beta02
-        maven("https://androidx.dev/snapshots/builds/11844073/artifacts/repository")
+        // TODO: Remove on Compose 1.8.0-alpha01
+        maven("https://androidx.dev/snapshots/builds/11876094/artifacts/repository")
     }
 }
 
