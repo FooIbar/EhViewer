d751b189369f24a50da3f1e41f67f07b9a25db6e
FooIbar <118464521+FooIbar@users.noreply.github.com>
2024-06-08 22:39:02 +0800

    Don't build libnettle at configuration time

--- app/src/main/cpp/build_nettle.sh
+++
@@ -1,8 +0,0 @@
-TOOLCHAIN=$ANDROID_HOME/ndk/$2/toolchains/llvm/prebuilt/$(uname -s | tr '[:upper:]' '[:lower:]')-x86_64/bin
-TARGET=$3
-patch -p1 < $1 \
-  && ./configure --host=$TARGET --disable-public-key --disable-dependency-tracking \
-  AR=$TOOLCHAIN/llvm-ar \
-  CC="$TOOLCHAIN/clang -target $TARGET" \
-  RANLIB=$TOOLCHAIN/llvm-ranlib \
-  && make libnettle.a CFLAGS="$4"
