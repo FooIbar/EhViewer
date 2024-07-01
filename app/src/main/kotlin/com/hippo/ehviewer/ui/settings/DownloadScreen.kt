ea523d7d41b8a8707f21eab18c92eaca067592b6
FooIbar <118464521+FooIbar@users.noreply.github.com>
2024-07-01 22:50:32 +0800

    Rescan download directory when restoring downloads

--- app/src/main/kotlin/com/hippo/ehviewer/ui/settings/DownloadScreen.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/ui/settings/DownloadScreen.kt
@@ -42,6 +42,7 @@ import com.hippo.ehviewer.dao.DownloadInfo
 import com.hippo.ehviewer.download.DownloadManager
 import com.hippo.ehviewer.download.downloadDir
 import com.hippo.ehviewer.download.downloadLocation
+import com.hippo.ehviewer.download.invalidateDownloadDirCache
 import com.hippo.ehviewer.spider.COMIC_INFO_FILE
 import com.hippo.ehviewer.spider.SpiderDen
 import com.hippo.ehviewer.spider.SpiderQueen.Companion.SPIDER_INFO_FILENAME
@@ -223,6 +224,8 @@ fun DownloadScreen(navigator: DestinationsNavigator) {
                     }.getOrNull()
                 }
                 runCatching {
+                    // Rescan download directory for new files added by the user
+                    invalidateDownloadDirCache()
                     val result = downloadLocation.listFiles().parMapNotNull { getRestoreItem(it) }.also {
                         fillGalleryListByApi(it, EhUrl.referer)
                     }
