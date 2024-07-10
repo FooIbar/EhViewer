7e1b3abcf68a6a29eba568d25b5b5cd804656a04
FooIbar <118464521+FooIbar@users.noreply.github.com>
2024-07-10 22:12:56 +0800

    Prepare for release 1.11.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,9 +71,9 @@ android {
         applicationId = "moe.tarsin.ehviewer"
         minSdk = 26
         targetSdk = 35
-        versionCode = 180057
-        versionName = "1.12.0"
-        versionNameSuffix = "-SNAPSHOT"
+        versionCode = 180058
+        versionName = "1.11.7"
+        // versionNameSuffix = "-SNAPSHOT"
         resourceConfigurations.addAll(
             listOf(
                 "zh",
