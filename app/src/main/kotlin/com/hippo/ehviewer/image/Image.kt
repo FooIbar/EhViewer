ab2e9963cf9f11f460108d1b45f0799164ca348d
Dude so hot <djohn@fbi.gov>
2024-09-10 15:18:15 +0800

    Debug: enable by default

--- app/src/main/kotlin/com/hippo/ehviewer/image/Image.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/image/Image.kt
@@ -34,6 +34,7 @@ import coil3.size.Dimension
 import coil3.size.Precision
 import com.hippo.ehviewer.Settings
 import com.hippo.ehviewer.coil.BitmapImageWithExtraInfo
+import com.hippo.ehviewer.coil.detectQRCode
 import com.hippo.ehviewer.coil.hardwareThreshold
 import com.hippo.ehviewer.coil.maybeCropBorder
 import com.hippo.ehviewer.jni.isGif
@@ -89,6 +90,7 @@ class Image private constructor(image: CoilImage, private val src: ImageSource) 
                 allowHardware(false)
                 hardwareThreshold(Settings.hardwareBitmapThreshold)
                 maybeCropBorder(Settings.cropBorder.value)
+                detectQRCode(true)
                 memoryCachePolicy(CachePolicy.DISABLED)
             }
             return when (val result = appCtx.imageLoader.execute(req)) {
