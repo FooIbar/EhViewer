3fd773422d3ae440a6466e2fa161a45018fd8ed7
Dude so hot <djohn@fbi.gov>
2024-09-14 18:20:52 +0800

    Respect Arch's ndk location

--- app/src/main/cpp/configure_nettle.sh
+++ app/src/main/cpp/configure_nettle.sh
@@ -1,4 +1,4 @@
-TOOLCHAIN=$ANDROID_HOME/ndk/$1/toolchains/llvm/prebuilt/$(uname -s | tr '[:upper:]' '[:lower:]')-x86_64/bin
+TOOLCHAIN=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/$(uname -s | tr '[:upper:]' '[:lower:]')-x86_64/bin
 TARGET=$2
 ./configure --host=$TARGET --disable-public-key --disable-dependency-tracking \
   AR=$TOOLCHAIN/llvm-ar \
