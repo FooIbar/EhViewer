30b4064e99c2877baff32da3330b0f5075cf9ea0
Dude so hot <djohn@fbi.gov>
2024-09-19 15:50:22 +0800

    Address code review

--- app/src/main/kotlin/com/hippo/ehviewer/coil/CropBorderInterceptor.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/coil/CropBorderInterceptor.kt
@@ -1,9 +1,8 @@
 package com.hippo.ehviewer.coil
 
 import android.graphics.Bitmap
-import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.IntRect
 import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.toIntRect
 import coil3.asImage
 import coil3.intercept.Interceptor
 import coil3.request.ImageResult
@@ -20,9 +19,10 @@ object CropBorderInterceptor : Interceptor {
                 if (image.rect.size != srcSize) {
                     val (x, y) = image.rect.topLeft
                     val (w, h) = image.rect.size
-                    Bitmap.createBitmap(image.image.bitmap, x, y, w, h).also { cropped ->
-                        image.image.bitmap.recycle()
-                        return result.copy(image = image.copy(image = cropped.asImage(), rect = IntRect(IntOffset.Zero, image.rect.size)))
+                    val src = image.image.bitmap
+                    Bitmap.createBitmap(src, x, y, w, h).also { cropped ->
+                        src.recycle()
+                        return result.copy(image = image.copy(image = cropped.asImage(), rect = image.rect.size.toIntRect()))
                     }
                 }
             }
