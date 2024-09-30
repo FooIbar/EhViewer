d162be547e5f6921c941de2a553b06c8e5b5ff63
Dude so hot <djohn@fbi.gov>
2024-09-30 09:24:02 +0800

    Cleanup with `getOrPut`

--- app/src/main/kotlin/com/hippo/ehviewer/spider/SpiderQueen.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/spider/SpiderQueen.kt
@@ -17,7 +17,6 @@
 package com.hippo.ehviewer.spider
 
 import androidx.annotation.IntDef
-import androidx.collection.LongSparseArray
 import androidx.collection.set
 import arrow.core.partially1
 import com.hippo.ehviewer.R
@@ -473,11 +472,11 @@ class SpiderQueen private constructor(val galleryInfo: GalleryInfo) : CoroutineS
         const val STATE_FINISHED = 2
         const val STATE_FAILED = 3
         const val SPIDER_INFO_FILENAME = ".ehviewer"
-        private val sQueenMap = LongSparseArray<SpiderQueen>()
+        private val sQueenMap = mutableMapOf<Long, SpiderQueen>()
 
         fun obtainSpiderQueen(galleryInfo: GalleryInfo, @Mode mode: Int): SpiderQueen {
             val gid = galleryInfo.gid
-            return (sQueenMap[gid] ?: SpiderQueen(galleryInfo).also { sQueenMap[gid] = it }).apply {
+            return (sQueenMap.getOrPut(gid) { SpiderQueen(galleryInfo) }).apply {
                 setMode(mode)
                 launch { updateMode() }
             }
