63765b1eb7ed9ca49c0cc3b7b55e539bf3f8441d
Dude so hot <djohn@fbi.gov>
2024-10-13 11:59:05 +0800

    More UX friendly

--- app/src/main/kotlin/com/hippo/ehviewer/ui/login/WebViewSignInScreen.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/ui/login/WebViewSignInScreen.kt
@@ -1,6 +1,7 @@
 package com.hippo.ehviewer.ui.login
 
 import android.webkit.WebView
+import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
@@ -12,13 +13,18 @@ import com.google.accompanist.web.rememberWebViewState
 import com.hippo.ehviewer.client.EhCookieStore
 import com.hippo.ehviewer.client.EhUrl
 import com.hippo.ehviewer.client.EhUtils
+import com.hippo.ehviewer.ui.composing
+import com.hippo.ehviewer.util.bgWork
 import com.hippo.ehviewer.util.setDefaultSettings
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootGraph
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import kotlinx.coroutines.awaitCancellation
+import kotlinx.coroutines.launch
 
 @Destination<RootGraph>
 @Composable
-fun WebViewSignInScreen() {
+fun AnimatedVisibilityScope.WebViewSignInScreen(navigator: DestinationsNavigator) = composing(navigator) {
     val state = rememberWebViewState(url = EhUrl.URL_SIGN_IN)
     val client = remember {
         object : AccompanistWebViewClient() {
@@ -26,6 +32,7 @@ fun WebViewSignInScreen() {
                 if (EhCookieStore.hasSignedIn()) {
                     postLogin()
                     view.destroy()
+                    launch { bgWork { awaitCancellation() } }
                 }
             }
         }
