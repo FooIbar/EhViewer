fba910e0e308d0a8598d89fa90ad278f8247a855
FooIbar <118464521+FooIbar@users.noreply.github.com>
2024-10-15 23:52:01 +0800

    Update Compose Material3 to 1.4

--- app/src/main/kotlin/com/hippo/ehviewer/ui/theme/Theme.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/ui/theme/Theme.kt
@@ -3,6 +3,7 @@ package com.hippo.ehviewer.ui.theme
 import androidx.compose.foundation.scrollbar.LocalScrollbarStyle
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MotionScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -46,7 +47,7 @@ fun EhTheme(useDarkTheme: Boolean, content: @Composable () -> Unit) {
         }
     }
 
-    MaterialTheme(colorScheme = colors) {
+    MaterialTheme(colorScheme = colors, motionScheme = CustomMotionScheme) {
         val scrollbarStyle = scrollbarStyle(color = MaterialTheme.colorScheme.primary)
         CompositionLocalProvider(
             LocalContentColor provides MaterialTheme.colorScheme.onBackground,
@@ -54,4 +55,21 @@ fun EhTheme(useDarkTheme: Boolean, content: @Composable () -> Unit) {
             content = content,
         )
     }
+}
+
+// https://issuetracker.google.com/363892346
+object CustomMotionScheme : MotionScheme {
+    private val motionScheme = MotionScheme.standardMotionScheme()
+
+    override fun <T> defaultSpatialSpec() = motionScheme.defaultEffectsSpec<T>()
+
+    override fun <T> fastSpatialSpec() = motionScheme.fastSpatialSpec<T>()
+
+    override fun <T> slowSpatialSpec() = motionScheme.slowSpatialSpec<T>()
+
+    override fun <T> defaultEffectsSpec() = motionScheme.defaultEffectsSpec<T>()
+
+    override fun <T> fastEffectsSpec() = motionScheme.fastEffectsSpec<T>()
+
+    override fun <T> slowEffectsSpec() = motionScheme.slowEffectsSpec<T>()
 }
