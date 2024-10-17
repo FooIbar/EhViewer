24a7957d50445ba1deb990e726ea56627853a76c
FooIbar <118464521+FooIbar@users.noreply.github.com>
2024-10-17 23:43:28 +0800

    Use Apache5 as fallback HTTP engine

---
+++ app/src/main/kotlin/com/hippo/ehviewer/ktor/CronetExtensions.kt
@@ -0,0 +1,27 @@
+@file:RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+
+package com.hippo.ehviewer.ktor
+
+import android.net.http.HttpEngine
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import com.hippo.ehviewer.Settings
+import java.io.File
+import splitties.init.appCtx
+
+fun CronetConfig.configureClient() {
+    context = appCtx
+    config = {
+        setEnableBrotli(true)
+        setUserAgent(Settings.userAgent)
+
+        // Cache Quic hint only since the real cache mechanism should on Ktor layer
+        val cache = File(appCtx.cacheDir, "http_cache").apply { mkdirs() }
+        setStoragePath(cache.path)
+        setEnableHttpCache(HttpEngine.Builder.HTTP_CACHE_DISK_NO_HTTP, 4096)
+
+        addQuicHint("e-hentai.org", 443, 443)
+        addQuicHint("forums.e-hentai.org", 443, 443)
+        addQuicHint("exhentai.org", 443, 443)
+    }
+}
