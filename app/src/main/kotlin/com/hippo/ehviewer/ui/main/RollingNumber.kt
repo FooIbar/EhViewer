6270b0d4d7ab477d21e11a6aeae87c4269902928
Dude so hot <djohn@fbi.gov>
2024-10-17 13:27:35 +0800

    Improve comments

--- app/src/main/kotlin/com/hippo/ehviewer/ui/main/RollingNumber.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/ui/main/RollingNumber.kt
@@ -36,6 +36,62 @@ import kotlin.math.floor
 import kotlin.math.max
 import kotlin.math.sin
 
+// We have three space to make animation looks continuous
+// 1. Concept space, where we have elements 0 1 2 3 4 5 6 7 8 9 null
+//    0, 9, null is connected and the have same norm(distance) to each other
+// 2. Intermediate, which is 2d euclidean space
+//    0 ~ 9 in Concept space is mapped to a circle rigidly
+//    null is mapped to a special point(0, - 1.528f * radius)
+//    keeping null - 0, null - 9, 0 - 9 have same distance
+// 3. Number Offset Space
+//    value is from -1 ~ 10, where -1 and 10 shows nothing
+//    radian of point in Intermediate space is mapped to here discontinuously
+
+// This controls animation speed
+private const val radius = 10f
+
+// Gaps between number in radian
+private const val gap = (2 * PI / 10).toFloat()
+private const val zeroDegree = -(PI / 2 + gap / 2).toFloat()
+
+// null is mapped to (0, - 1.528f * radius)
+private const val specialNodeDistanceFactor = 1.528f
+
+// Input: Concept space: 0 1 2 3 4 6 6 7 8 9 null
+// Output: Position in 2d euclidean space partial circle
+private fun conceptSpaceToIntermediate(value: Int?): Offset {
+    if (value != null) {
+        val theta = zeroDegree - value * gap
+        return Offset(cos(theta), sin(theta)) * radius
+    } else {
+        return Offset(0f, -specialNodeDistanceFactor) * radius
+    }
+}
+
+// Output: -1 ~ 10, where -1 and 10 shows nothing
+private fun intermediateToNumberOffset(circleOffset: Offset): Float {
+    val degree = atan2(circleOffset.y, circleOffset.x)
+    val normalized = normalize(zeroDegree - degree)
+    return if (normalized > 9 * gap) {
+        val reNormalized = normalized - 9 * gap - gap / 2
+        if (reNormalized > 0) {
+            // 0 side
+            reNormalized / gap * 2 - 1
+        } else {
+            // 9 side
+            10 + reNormalized / gap * 2
+        }
+    } else {
+        normalized / gap
+    }
+}
+
+// Normalize radian to [0, 2 * PI]
+private fun normalize(degree: Float) = (degree - 2 * PI * floor(degree / (2 * PI))).toFloat()
+
+// Convert 2d euclidean space partial circle to UI Offset
+private fun numberOffsetToUIOffset(size: IntSize, number: Float) = IntOffset(0, -(size.height * 2 * number).toInt())
+
 @Composable
 fun RollingNumber(number: Int, style: TextStyle = LocalTextStyle.current, width: Int? = null) {
     val string = remember(number) { "$number" }
@@ -70,12 +126,12 @@ fun RollingNumber(number: Int, style: TextStyle = LocalTextStyle.current, width:
                     val absent = max - len
                     val where = max - reversed - 1
                     val v = if (where < absent) null else str[where - absent].digitToInt()
-                    mapSimplyConnectedElementToEuclideanPartialCircle(v)
+                    conceptSpaceToIntermediate(v)
                 }
                 Column(
                     modifier = Modifier.offset {
-                        val number = partialCircleToLinearUIOffset(rotate)
-                        numberToOffset(size, 9 - number)
+                        val number = intermediateToNumberOffset(rotate)
+                        numberOffsetToUIOffset(size, 9 - number)
                     },
                     horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
@@ -106,49 +162,3 @@ fun RollingNumber(number: Int, style: TextStyle = LocalTextStyle.current, width:
 }
 
 private const val maxNumber = 5
-
-// This factor controls animation speed
-private const val factor = 10f
-
-private const val gap = (2 * PI / 10).toFloat()
-private const val zeroDegree = -(PI / 2 + gap / 2).toFloat()
-
-// A special factor depends on gaps to keep 0 ~ null, null ~ 9 have same distance
-private const val specialNodeDistanceFactor = 1.528f
-
-// Convert a number which belongs **1-connected** animation space to 2d euclidean space
-// Input 1-connected space: 0 1 2 3 4 6 6 7 8 9 null. where 0 is connected to null, so **1-connected**
-// Output: partial circle in 2d euclidean space, the radius is controlled by [factor]
-// but null is always mapped to (0, 2 * factor), and keep same norm (distance) with 0 and 9
-private fun mapSimplyConnectedElementToEuclideanPartialCircle(value: Int?): Offset {
-    if (value != null) {
-        val theta = zeroDegree - value * gap
-        return Offset(cos(theta), sin(theta)) * factor
-    } else {
-        return Offset(0f, -specialNodeDistanceFactor) * factor
-    }
-}
-
-// Output: -1 ~ 10, where -1 and 10 shows nothing
-private fun partialCircleToLinearUIOffset(circleOffset: Offset): Float {
-    val degree = atan2(circleOffset.y, circleOffset.x)
-    val normalized = normalize(zeroDegree - degree)
-    return if (normalized > 9 * gap) {
-        val reNormalized = normalized - 9 * gap - gap / 2
-        if (reNormalized > 0) {
-            // 0 side
-            reNormalized / gap * 2 - 1
-        } else {
-            // 9 side
-            10 + reNormalized / gap * 2
-        }
-    } else {
-        normalized / gap
-    }
-}
-
-// Normalize radian to [0, 2 * PI]
-private fun normalize(degree: Float) = (degree - 2 * PI * floor(degree / (2 * PI))).toFloat()
-
-// Convert 2d euclidean space partial circle to UI Offset
-private fun numberToOffset(size: IntSize, number: Float) = IntOffset(0, -(size.height * 2 * number).toInt())
