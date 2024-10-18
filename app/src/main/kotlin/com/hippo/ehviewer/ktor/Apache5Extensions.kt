94ec01e999613898fc652576acd077f6efa63aee
Dude so hot <djohn@fbi.gov>
2024-10-18 20:04:34 +0800

    Apache5 HC use Android DnsResolver

--- app/src/main/kotlin/com/hippo/ehviewer/ktor/Apache5Extensions.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/ktor/Apache5Extensions.kt
@@ -1,8 +1,23 @@
 package com.hippo.ehviewer.ktor
 
+import android.net.DnsResolver
+import android.os.Build
+import androidx.annotation.RequiresApi
+import arrow.fx.coroutines.parZip
 import com.hippo.ehviewer.util.isAtLeastQ
 import io.ktor.client.engine.apache5.Apache5EngineConfig
+import java.net.InetAddress
+import java.net.UnknownHostException
 import java.security.Security
+import java.util.concurrent.Executor
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.asExecutor
+import kotlinx.coroutines.runBlocking
+import moe.tarsin.kt.unreachable
+import org.apache.hc.client5.http.DnsResolver as ApacheDnsResolver
 import org.apache.hc.client5.http.config.ConnectionConfig
 import org.apache.hc.client5.http.impl.nio.PoolingAsyncClientConnectionManagerBuilder
 import org.apache.hc.core5.reactor.IOReactorConfig
@@ -16,7 +31,7 @@ fun Apache5EngineConfig.configureClient() {
     customizeClient {
         setConnectionManager(
             PoolingAsyncClientConnectionManagerBuilder.create().apply {
-                setMaxConnPerRoute(2)
+                setMaxConnPerRoute(1)
                 setDefaultConnectionConfig(
                     ConnectionConfig.custom().apply {
                         setConnectTimeout(Timeout.ofMilliseconds(connectTimeout))
@@ -28,7 +43,49 @@ fun Apache5EngineConfig.configureClient() {
                         setIoThreadCount(1)
                     }.build(),
                 )
+                if (isAtLeastQ) {
+                    val resolver = DnsResolver.getInstance()
+                    val executor = Dispatchers.IO.asExecutor()
+                    setDnsResolver(
+                        object : ApacheDnsResolver {
+                            override fun resolve(host: String) = query(host, resolver, executor).toTypedArray()
+                            override fun resolveCanonicalHostname(host: String) = unreachable()
+                        },
+                    )
+                }
             }.build(),
         )
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.Q)
+fun query(host: String, resolver: DnsResolver, executor: Executor) = runBlocking {
+    suspend fun doQuery(type: Int) = suspendCoroutine { cont ->
+        resolver.query(
+            null,
+            host,
+            type,
+            DnsResolver.FLAG_EMPTY,
+            executor,
+            null,
+            object : DnsResolver.Callback<List<InetAddress>> {
+                override fun onAnswer(result: List<InetAddress>, r: Int) {
+                    cont.resume(result)
+                }
+                override fun onError(e: DnsResolver.DnsException) {
+                    cont.resumeWithException(e)
+                }
+            },
+        )
+    }
+
+    try {
+        parZip(
+            { doQuery(DnsResolver.TYPE_A) },
+            { doQuery(DnsResolver.TYPE_AAAA) },
+            { a, b -> (a + b).shuffled() },
+        )
+    } catch (e: Exception) {
+        throw UnknownHostException(e.message).apply { initCause(e) }
     }
 }
