ce8e91aab85a405963767b573cba15d977c3d741
FooIbar <118464521+FooIbar@users.noreply.github.com>
2024-11-01 21:03:47 +0800

    Prepare for next iteration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,8 +79,8 @@ android {
     defaultConfig {
         applicationId = "moe.tarsin.ehviewer"
         versionCode = 180061
-        versionName = "1.13.0"
-        // versionNameSuffix = "-SNAPSHOT"
+        versionName = "1.14.0"
+        versionNameSuffix = "-SNAPSHOT"
         buildConfigField("String", "RAW_VERSION_NAME", "\"$versionName${versionNameSuffix.orEmpty()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"$commitSha\"")
         buildConfigField("long", "COMMIT_TIME", commitTime)
