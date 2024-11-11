571969fca92fa5fc015355dbd2dd8b0d36702e6e
FooIbar <118464521+FooIbar@users.noreply.github.com>
2024-11-11 21:14:29 +0800

    Prepare for release 1.13.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,9 +78,9 @@ android {
 
     defaultConfig {
         applicationId = "moe.tarsin.ehviewer"
-        versionCode = 180061
-        versionName = "1.14.0"
-        versionNameSuffix = "-SNAPSHOT"
+        versionCode = 180062
+        versionName = "1.13.1"
+        // versionNameSuffix = "-SNAPSHOT"
         buildConfigField("String", "RAW_VERSION_NAME", "\"$versionName${versionNameSuffix.orEmpty()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"$commitSha\"")
         buildConfigField("long", "COMMIT_TIME", commitTime)
