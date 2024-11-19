c648a7789ec803cafa41f9e4addfee849e35c301
Dude so hot <djohn@fbi.gov>
2024-11-19 14:14:20 +0800

    PrefetchAround for GalleryList

--- app/src/main/kotlin/com/hippo/ehviewer/coil/Extensions.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/coil/Extensions.kt
@@ -1,5 +1,8 @@
 package com.hippo.ehviewer.coil
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.paging.compose.LazyPagingItems
 import coil3.decode.BlackholeDecoder
 import coil3.request.CachePolicy
 import coil3.request.ImageRequest
@@ -10,6 +13,7 @@ import com.hippo.ehviewer.client.data.V2GalleryPreview
 import com.hippo.ehviewer.client.thumbUrl
 import com.hippo.ehviewer.dao.DownloadInfo
 import com.hippo.ehviewer.download.downloadLocation
+import com.hippo.ehviewer.ktbuilder.execute
 
 fun ImageRequest.Builder.ehUrl(info: GalleryInfo) = apply {
     val key = info.thumbKey!!
@@ -35,4 +39,18 @@ fun ImageRequest.Builder.ehPreview(preview: GalleryPreview) = apply {
 fun ImageRequest.Builder.justDownload() = apply {
     memoryCachePolicy(CachePolicy.DISABLED)
     decoderFactory(BlackholeDecoder.Factory())
+}
+
+@Composable
+inline fun <T : Any> PrefetchAround(data: LazyPagingItems<T>, index: Int, distance: Int, crossinline f: suspend (T) -> ImageRequest) {
+    data.peek((index - distance).coerceAtLeast(0))?.let { fetchBefore ->
+        LaunchedEffect(fetchBefore) {
+            f(fetchBefore).execute()
+        }
+    }
+    data.peek((index + distance).coerceAtMost(data.itemCount - 1))?.let { fetchAhead ->
+        LaunchedEffect(fetchAhead) {
+            f(fetchAhead).execute()
+        }
+    }
 }
