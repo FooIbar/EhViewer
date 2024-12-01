8ae62e21a7d9793d1e77deb1ea9e7fcd480abf5c
FooIbar <118464521+FooIbar@users.noreply.github.com>
2024-12-01 14:15:30 +0800

    Clean up redundant v1 thumbnails

--- app/src/main/kotlin/com/hippo/ehviewer/coil/DownloadThumbInterceptor.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/coil/DownloadThumbInterceptor.kt
@@ -8,6 +8,7 @@ import coil3.request.ImageResult
 import coil3.request.SuccessResult
 import com.hippo.ehviewer.EhApplication.Companion.imageCache
 import com.hippo.ehviewer.util.sendTo
+import com.hippo.files.delete
 import com.hippo.files.isDirectory
 import com.hippo.files.isFile
 import com.hippo.files.toUri
@@ -26,22 +27,29 @@ val ImageRequest.downloadLocation: Path
 
 object DownloadThumbInterceptor : Interceptor {
     override suspend fun intercept(chain: Interceptor.Chain): ImageResult {
-        val thumb = chain.request.downloadLocation
-        if (thumb != emptyPath) {
+        val dir = chain.request.downloadLocation
+        if (dir != emptyPath) {
+            val format = (chain.request.data as String).substringAfterLast('.', "")
+            check(format.isNotBlank())
+            val thumb = dir / "thumb.$format"
+            val v1Thumb = dir / "thumb.jpg"
             if (thumb.isFile) {
                 val new = chain.request.newBuilder().data(thumb.toUri()).build()
                 val result = chain.withRequest(new).proceed()
-                if (result is SuccessResult) return result
+                if (result is SuccessResult) {
+                    if (thumb != v1Thumb) v1Thumb.delete()
+                    return result
+                }
             }
-            val new = chain.request.newBuilder().build()
-            val result = chain.withRequest(new).proceed()
-            if (result is SuccessResult && thumb.parent?.isDirectory == true) {
+            val result = chain.proceed()
+            if (result is SuccessResult && dir.isDirectory) {
                 // Accessing the recreated file immediately after deleting it throws
                 // FileNotFoundException, so we just overwrite the existing file.
                 val key = requireNotNull(chain.request.memoryCacheKey)
                 imageCache.read(key) {
                     data sendTo thumb
                 }
+                if (thumb != v1Thumb) v1Thumb.delete()
             }
             return result
         }
