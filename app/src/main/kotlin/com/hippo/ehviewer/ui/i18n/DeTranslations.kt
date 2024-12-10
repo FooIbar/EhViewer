aec135cbe167896d018b7a3739dcde33b70d3924
Dude so hot <djohn@fbi.gov>
2024-12-10 14:40:17 +0800

    Cleanup

--- app/src/main/kotlin/com/hippo/ehviewer/ui/i18n/DeTranslations.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/ui/i18n/DeTranslations.kt
@@ -157,23 +157,11 @@ object DeTranslations : Translations by EnTranslations {
     override val statDownloadDoneTextSucceeded = { a: Int -> "$a Download(s) wurde(n) abgeschlossen" }
     override val statDownloadDoneTextFailed = { a: Int -> "$a Download(s) gescheitert" }
     override val statDownloadDoneTextMix = { a: Int, b: Int -> "$a erfolgreich abgeschlossen, $b gescheitert" }
-    override val statDownloadDoneLineSucceeded = { p0: String ->
-        "Abgeschlossen: %s"
-            .format(p0)
-    }
-    override val statDownloadDoneLineFailed = { p0: String ->
-        "Gescheitert: %s"
-            .format(p0)
-    }
+    override val statDownloadDoneLineSucceeded = { a: String -> "Abgeschlossen: $a" }
+    override val statDownloadDoneLineFailed = { a: String -> "Gescheitert: $a" }
     override val downloadRemoveDialogTitle = "Download entfernen"
-    override val downloadRemoveDialogMessage = { p0: String ->
-        "%s aus der Downloadliste entfernen?"
-            .format(p0)
-    }
-    override val downloadRemoveDialogMessage2 = { p0: Int ->
-        "%d Downloads aus der Downloadliste entfernen?"
-            .format(p0)
-    }
+    override val downloadRemoveDialogMessage = { a: String -> "$a aus der Downloadliste entfernen?" }
+    override val downloadRemoveDialogMessage2 = { a: Int -> "$a Downloads aus der Downloadliste entfernen?" }
     override val downloadRemoveDialogCheckText = "Bilddateien löschen"
     override val statDownloadActionStopAll = "Alle stopppen"
     override val defaultDownloadLabelName = "Standard"
@@ -182,14 +170,8 @@ object DeTranslations : Translations by EnTranslations {
     override val downloadStartAll = "Alle starten"
     override val downloadStopAll = "Alle stoppen"
     override val downloadServiceLabel = "EhViewer-Download-Dienst"
-    override val downloadSpeedText = { p0: String ->
-        "%s"
-            .format(p0)
-    }
-    override val downloadSpeedText2 = { p0: String, p1: String ->
-        "%s, noch %s übrig"
-            .format(p0, p1)
-    }
+    override val downloadSpeedText = { a: String -> a }
+    override val downloadSpeedText2 = { a: String, b: String -> "$a, noch $b übrig" }
     override val rememberDownloadLabel = "Download-Etikette merken"
     override val defaultDownloadLabel = "Standard-Download-Etikette"
     override val addedToDownloadList = "Zu Download-Liste hinzugefügt"
@@ -207,22 +189,15 @@ object DeTranslations : Translations by EnTranslations {
     override val filterUploader = "Uploader"
     override val filterTag = "Etikett"
     override val filterTagNamespace = "Etikettsnamensraum"
-    override val deleteFilter = { p0: String ->
-        "Blocker \"%s\" entfernen?"
-            .format(p0)
-    }
+    override val deleteFilter = { a: String -> "Blocker \"$a\" entfernen?" }
     override val showDefinition = "Definition anzeigen"
     override val addFilter = "Blocker erstellen"
     override val filterText = "Blocker-Text"
-    override val filterTip =
-        "Die blockierten Galerien wurde in EHentai nicht angezeigt.\n\nTitel-Blocker: die Galerien, deren Titel das Wort enthalten, ausschließen.\n\nUploader-Blocker: Die vom Uploader hochgeladen Galerien ausschließen.\n\nEtikett-Blocker: Die Galerien, die die Etikett haben, ausschließen. Es wird länger dauern, Galerieliste zu laden.\n\nEtikettnamensraum-Blocker: Die Galerien, die den Etikettnamensraum haben, ausschließen. Es wird länger dauern, Galerieliste zu laden."
+    override val filterTip = "Die blockierten Galerien wurde in EHentai nicht angezeigt.\n\nTitel-Blocker: die Galerien, deren Titel das Wort enthalten, ausschließen.\n\nUploader-Blocker: Die vom Uploader hochgeladen Galerien ausschließen.\n\nEtikett-Blocker: Die Galerien, die die Etikett haben, ausschließen. Es wird länger dauern, Galerieliste zu laden.\n\nEtikettnamensraum-Blocker: Die Galerien, die den Etikettnamensraum haben, ausschließen. Es wird länger dauern, Galerieliste zu laden."
     override val uConfig = "EHentai-Einstellungen"
     override val applyTip = "Zum Speichern der Einstellungen auf Häkchen tippen"
     override val shareImage = "Das Bild teilen"
-    override val imageSaved = { p0: String ->
-        "Das Bild wurde zu %s gespeichert"
-            .format(p0)
-    }
+    override val imageSaved = { a: String -> "Das Bild wurde zu $a gespeichert" }
     override val settingsEh = "EH"
     override val settingsEhSignOut = "Abmelden"
     override val settingsUConfig = "EHentai-Einstellungen"
@@ -238,87 +213,47 @@ object DeTranslations : Translations by EnTranslations {
     override val settingsEhDetailSizeLong = "Lange"
     override val settingsEhDetailSizeShort = "Kurz"
     override val settingsAdvancedAppLanguageTitle = "Sprache der App (Language)"
-    override val torrentCount = { p0: Int ->
-        "Torrent (%d)"
-            .format(p0)
-    }
+    override val torrentCount = { a: Int -> "Torrent ($a)" }
     override val settingsEhShowJpnTitle = "Japanische Titel anzeigen"
     override val settingsEhFilter = "Galerie-Blocker"
-    override val settingsEhFilterSummary =
-        "Nach Titel, Uploader order Etiketten die unerwünschten Galerien verstecken"
+    override val settingsEhFilterSummary = "Nach Titel, Uploader order Etiketten die unerwünschten Galerien verstecken"
     override val settingsDownload = "Download"
     override val settingsDownloadDownloadLocation = "Downloadziel"
-    override val settingsDownloadCantGetDownloadLocation =
-        "Downloadziel konnte nicht geladen werden"
+    override val settingsDownloadCantGetDownloadLocation = "Downloadziel konnte nicht geladen werden"
     override val settingsDownloadMediaScan = "Medienscan erlauben"
     override val settingsDownloadMediaScanSummaryOn = "Du solltest deine Galerie-Apps verstecken"
-    override val settingsDownloadMediaScanSummaryOff =
-        "Meisten Galerie-Apps ignorieren die Bilder im Downloadpfad"
+    override val settingsDownloadMediaScanSummaryOff = "Meisten Galerie-Apps ignorieren die Bilder im Downloadpfad"
     override val settingsDownloadConcurrency = "Multi-Thread-Download"
-    override val settingsDownloadConcurrencySummary = { p0: String ->
-        "Bis zu %s Bild(er)"
-            .format(p0)
-    }
+    override val settingsDownloadConcurrencySummary = { a: String -> "Bis zu $a Bild(er)" }
     override val settingsDownloadPreloadImage = "Bilder vorabladen"
-    override val settingsDownloadPreloadImageSummary = { p0: String ->
-        "Die nächste %s Bilder vorabladen"
-            .format(p0)
-    }
+    override val settingsDownloadPreloadImageSummary = { a: String -> "Die nächste $a Bilder vorabladen" }
     override val settingsDownloadDownloadOriginImage = "Originale Bilder herunterladen"
-    override val settingsDownloadDownloadOriginImageSummary =
-        "Wenn es aktiviert wird, wird der 509-Fehler dir schneller widerfahren"
+    override val settingsDownloadDownloadOriginImageSummary = "Wenn es aktiviert wird, wird der 509-Fehler dir schneller widerfahren"
     override val settingsDownloadRestoreDownloadItems = "Download-Elemente wiederherstellen"
-    override val settingsDownloadRestoreDownloadItemsSummary =
-        "Alle Download-Elemente im Downloadpfad wiederherstellen"
+    override val settingsDownloadRestoreDownloadItemsSummary = "Alle Download-Elemente im Downloadpfad wiederherstellen"
     override val settingsDownloadRestoreNotFound = "Keine zu wiederhestellenden Elemente"
     override val settingsDownloadRestoreFailed = "Wiederherstellung gescheitert"
-    override val settingsDownloadRestoreSuccessfully = { p0: Int ->
-        "%d Element(e) wiederhergestellt"
-            .format(p0)
-    }
+    override val settingsDownloadRestoreSuccessfully = { a: Int -> "$a Element(e) wiederhergestellt" }
     override val settingsDownloadCleanRedundancy = "Unnötige Dateien löschen"
-    override val settingsDownloadCleanRedundancySummary =
-        "Bilddateien, die sich im Downloadpfad befinden, aber nicht in Downloadliste, löschen"
-    override val settingsDownloadCleanRedundancyNoRedundancy =
-        "Keine zu löschenden Dateien gefunden"
-    override val settingsDownloadCleanRedundancyDone = { p0: Int ->
-        "Reinigung abgeschlossen. %d Datei(en) wurde(n) gelöscht."
-            .format(p0)
-    }
+    override val settingsDownloadCleanRedundancySummary = "Bilddateien, die sich im Downloadpfad befinden, aber nicht in Downloadliste, löschen"
+    override val settingsDownloadCleanRedundancyNoRedundancy = "Keine zu löschenden Dateien gefunden"
+    override val settingsDownloadCleanRedundancyDone = { a: Int -> "Reinigung abgeschlossen. $a Datei(en) wurde(n) gelöscht." }
     override val settingsAdvanced = "Erweiterte Optionen"
     override val settingsAdvancedSaveParseErrorBody = "HTML-Datei beim Parsing-Fehler speichern"
-    override val settingsAdvancedSaveParseErrorBodySummary =
-        "Die Datei könnte etwas Privates enthalten"
+    override val settingsAdvancedSaveParseErrorBodySummary = "Die Datei könnte etwas Privates enthalten"
     override val settingsAdvancedSaveCrashLog = "Crash-Bericht speichern"
-    override val settingsAdvancedSaveCrashLogSummary =
-        "Der Bericht hilft dem Entwickler dabei, Fehler schnell zu beheben."
+    override val settingsAdvancedSaveCrashLogSummary = "Der Bericht hilft dem Entwickler dabei, Fehler schnell zu beheben."
     override val settingsAdvancedDumpLogcat = "Logcat-Protokoll speichern"
-    override val settingsAdvancedDumpLogcatSummary =
-        "Logcat-Protokolldatei auf externem Speicher speichern"
+    override val settingsAdvancedDumpLogcatSummary = "Logcat-Protokolldatei auf externem Speicher speichern"
     override val settingsAdvancedDumpLogcatFailed = "Speichern gescheitert"
-    override val settingsAdvancedDumpLogcatTo = { p0: String ->
-        "Protokolldatei wurde auf %s gespeichert."
-            .format(p0)
-    }
+    override val settingsAdvancedDumpLogcatTo = { a: String -> "Protokolldatei wurde auf $a gespeichert." }
     override val cloudFavorites = "Cloud-Favoriten"
     override val localFavorites = "Lokale Favoriten"
-    override val searchBarHint = { p0: String ->
-        "%s durchsuchen"
-            .format(p0)
-    }
-    override val favoritesTitle = { p0: String ->
-        "%s"
-            .format(p0)
-    }
-    override val favoritesTitle2 = { p0: String, p1: String ->
-        "%s - %s"
-            .format(p0, p1)
-    }
+    override val searchBarHint = { a: String -> "$a durchsuchen" }
+    override val favoritesTitle = { a: String -> a }
+    override val favoritesTitle2 = { a: String, b: String -> "$a - $b" }
     override val deleteFavoritesDialogTitle = "Aus Favoriten löschen"
-    override val deleteFavoritesDialogMessage = { p0: Int ->
-        "%d Elemente aus Favoriten löschen?"
-            .format(p0)
-    }
+    override val deleteFavoritesDialogMessage = { a: Int -> "$a Elemente aus Favoriten löschen?" }
     override val moveFavoritesDialogTitle = "Favoriten verschieben"
     override val defaultFavoritesCollection = "Standard-Favoriten"
     override val letMeSelect = "Manuell auswählen"
@@ -327,32 +262,24 @@ object DeTranslations : Translations by EnTranslations {
     override val fromTheFuture = "Von der Zukunft"
     override val justNow = "Gerade"
     override val yesterday = "Gestern"
-    override val someDaysAgo = { p0: Int ->
-        "Vor %d Tagen"
-            .format(p0)
-    }
+    override val someDaysAgo = { a: Int -> "Vor $a Tagen" }
     override val archive = "Archivdateien"
     override val noArchives = "Keine Archivdatei"
     override val downloadArchiveStarted = "Archivdatei wird heruntergeladen"
     override val downloadArchiveFailure = "Fehler beim Herunterladen der Archivdatei"
-    override val downloadArchiveFailureNoHath =
-        "H@H-Klient ist für das Herunterladen der Archivdateien erforderlich"
+    override val downloadArchiveFailureNoHath = "H@H-Klient ist für das Herunterladen der Archivdateien erforderlich"
     override val settingsPrivacy = "Datenschutzoptionen"
     override val settingsPrivacySecure = "Screenshot verhindern"
-    override val settingsPrivacySecureSummary =
-        "Screenshots werden verhindert. Inhalt der App wird in Aktuelle Apps-Liste nicht angezeigt."
-    override val appWaring =
-        "Inhalt dieser App kommt aus dem Internet, von dem einige dir körperlichen oder emotionalen Schaden zufügen kann. Du hast es wirklich erfahrt und möchtest immer noch fortsetzen."
+    override val settingsPrivacySecureSummary = "Screenshots werden verhindert. Inhalt der App wird in Aktuelle Apps-Liste nicht angezeigt."
+    override val appWaring = "Inhalt dieser App kommt aus dem Internet, von dem einige dir körperlichen oder emotionalen Schaden zufügen kann. Du hast es wirklich erfahrt und möchtest immer noch fortsetzen."
     override val errorUsernameCannotEmpty = "Gibt den Nutzername ein"
     override val errorPasswordCannotEmpty = "Gibt das Passwort ein"
     override val guestMode = "Anmeldung überspringen"
     override val signInFailed = "Anmeldung fehlgeschlagen"
-    override val clipboardGalleryUrlSnackMessage =
-        "Ein Link von einen Galerie wurde in Zwischenablage erkennt."
+    override val clipboardGalleryUrlSnackMessage = "Ein Link von einen Galerie wurde in Zwischenablage erkennt."
     override val clipboardGalleryUrlSnackAction = "Gehen"
     override val settingsAdvancedReadCacheSize = "Größe des Lesezwischenspeichers"
-    override val settingsAboutDeclarationSummary =
-        "EhViewer verbindet sich in keiner Weise mit E-Hentai.org."
+    override val settingsAboutDeclarationSummary = "EhViewer verbindet sich in keiner Weise mit E-Hentai.org."
     override val settingsAboutSource = "Quellcode"
     override val settingsAboutVersion = "Versionnummer"
     override val settingsAboutCheckForUpdates = "Auf Update überprüfen"
@@ -360,12 +287,8 @@ object DeTranslations : Translations by EnTranslations {
     override val appLanguageSystem = "Sprache des Systems (Standard)"
     override val pleaseWait = "Bitte warte"
     override val settingsAdvancedExportData = "Daten exportieren"
-    override val settingsAdvancedExportDataSummary =
-        "Daten, z. B. Downloadliste, Sofortsucheliste, zum externen Speicher speichern"
-    override val settingsAdvancedExportDataTo = { p0: String ->
-        "Daten wurden zu %s exportiert"
-            .format(p0)
-    }
+    override val settingsAdvancedExportDataSummary = "Daten, z. B. Downloadliste, Sofortsucheliste, zum externen Speicher speichern"
+    override val settingsAdvancedExportDataTo = { a: String -> "Daten wurden zu $a exportiert" }
     override val settingsAdvancedExportDataFailed = "Datenexport gescheitert"
     override val settingsAdvancedImportData = "Daten importieren"
     override val settingsAdvancedImportDataSummary = "Gespeicherte Daten wiederherstellen"
@@ -374,13 +297,10 @@ object DeTranslations : Translations by EnTranslations {
     override val settingsAboutAuthor = "Entwickler"
     override val settingsAboutLatestRelease = "Aktuelle Version"
     override val downloadResetReadingProgress = "Lesefortschritte zurücksetzen"
-    override val resetReadingProgressMessage =
-        "Lesefortschritte aus allen heruntergeladenen Galerie zurücksetzen?"
-    override val settingsEhIdentityCookiesSigned =
-        "Wenn jemand die folgenden Informationen hat, kann er sich als du anmelden. <br><b>ALSO GEHEIM HALTEN!</b>"
+    override val resetReadingProgressMessage = "Lesefortschritte aus allen heruntergeladenen Galerie zurücksetzen?"
+    override val settingsEhIdentityCookiesSigned = "Wenn jemand die folgenden Informationen hat, kann er sich als du anmelden. <br><b>ALSO GEHEIM HALTEN!</b>"
     override val settingsEhIdentityCookiesGuest = "Du hast dich noch nicht angemeldet."
     override val settingsEhShowGalleryPages = "Galerieseitenzahl anzeigen"
-    override val settingsEhShowGalleryPagesSummary =
-        "Die Galerieseitenzahle in der Galerieliste anzeigen"
+    override val settingsEhShowGalleryPagesSummary = "Die Galerieseitenzahle in der Galerieliste anzeigen"
     override val darkThemeFollowSystem = "System folgen"
 }
