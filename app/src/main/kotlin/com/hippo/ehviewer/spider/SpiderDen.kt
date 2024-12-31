309da835a8ba1deacce8135f82a23c81b6d58e5b
Dude so hot <djohn@fbi.gov>
2024-12-31 14:51:29 +0800

    Extract common part

--- app/src/main/kotlin/com/hippo/ehviewer/spider/SpiderDen.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/spider/SpiderDen.kt
@@ -28,7 +28,6 @@ import com.hippo.ehviewer.client.EhUtils.getSuitableTitle
 import com.hippo.ehviewer.client.data.GalleryDetail
 import com.hippo.ehviewer.client.data.GalleryInfo
 import com.hippo.ehviewer.client.ehRequest
-import com.hippo.ehviewer.client.executeSafely
 import com.hippo.ehviewer.client.getImageKey
 import com.hippo.ehviewer.coil.read
 import com.hippo.ehviewer.coil.suspendEdit
@@ -38,11 +37,9 @@ import com.hippo.ehviewer.download.downloadLocation
 import com.hippo.ehviewer.download.tempDownloadDir
 import com.hippo.ehviewer.image.PathSource
 import com.hippo.ehviewer.jni.archiveFdBatch
-import com.hippo.ehviewer.ui.tools.SpeedTracker
+import com.hippo.ehviewer.ui.tools.timeoutBySpeed
 import com.hippo.ehviewer.util.FileUtils
-import com.hippo.ehviewer.util.LowSpeedException
 import com.hippo.ehviewer.util.copyTo
-import com.hippo.ehviewer.util.ensureSuccess
 import com.hippo.ehviewer.util.sendTo
 import com.hippo.ehviewer.util.sha1
 import com.hippo.files.delete
@@ -54,21 +51,13 @@ import com.hippo.files.mkdirs
 import com.hippo.files.moveTo
 import com.hippo.files.openFileDescriptor
 import eu.kanade.tachiyomi.util.system.logcat
-import io.ktor.client.plugins.onDownload
-import io.ktor.client.plugins.timeout
 import io.ktor.client.statement.HttpResponse
 import io.ktor.client.statement.bodyAsChannel
 import io.ktor.client.statement.request
-import io.ktor.utils.io.copyTo
 import java.util.concurrent.locks.ReentrantReadWriteLock
 import kotlin.concurrent.read
 import kotlin.concurrent.write
-import kotlin.time.Duration.Companion.seconds
 import kotlinx.coroutines.CancellationException
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import okio.Path
 
 class SpiderDen(val info: GalleryInfo) {
@@ -176,41 +165,11 @@ class SpiderDen(val info: GalleryInfo) {
         url: String,
         referer: String?,
         notifyProgress: (Long, Long, Int) -> Unit,
-    ) {
-        val tracker = SpeedTracker(2.seconds)
-        return ehRequest(url, referer) {
-            var prev = 0L
-            onDownload { done, total ->
-                val bytesRead = (done - prev).toInt()
-                tracker.track(bytesRead)
-                notifyProgress(total!!, done, bytesRead)
-                prev = done
-            }
-            timeout { connectTimeoutMillis = Settings.connTimeout * 1000L }
-        }.executeSafely { resp ->
-            resp.status.ensureSuccess()
-            val timeoutSpeed = Settings.timeoutSpeed * 1024.0
-            coroutineScope {
-                val work = async { check(saveFromHttpResponse(index, resp)) }
-                val job = launch {
-                    delay(4.seconds) // Tolerant 4 secs for receiving
-                    tracker.speedFlow(2.seconds).collect { speed ->
-                        if (timeoutSpeed != 0.0 && speed < timeoutSpeed) {
-                            throw LowSpeedException(
-                                resp.request.url.toString(),
-                                speed.toLong(),
-                            )
-                        }
-                    }
-                }
-                try {
-                    work.await()
-                } finally {
-                    job.cancel()
-                }
-            }
-        }
-    }
+    ) = timeoutBySpeed(
+        { ehRequest(url, referer, builder = it) },
+        { a, b, c -> notifyProgress(a, b, c) },
+        { resp -> check(saveFromHttpResponse(index, resp)) },
+    )
 
     private suspend inline fun saveResponseMeta(
         index: Int,
