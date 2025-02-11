db064e788b74c30b5e513ba10a42346f3e59be49
FooIbar <118464521+FooIbar@users.noreply.github.com>
2025-02-11 21:43:23 +0800

    Fix auto logout when logging in through WebView

--- app/src/main/kotlin/com/hippo/ehviewer/ui/login/WebViewSignInScreen.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/ui/login/WebViewSignInScreen.kt
@@ -4,10 +4,9 @@ import android.webkit.WebView
 import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
-import com.google.accompanist.web.AccompanistWebViewClient
 import com.google.accompanist.web.WebView
 import com.google.accompanist.web.rememberWebViewState
 import com.hippo.ehviewer.client.EhCookieStore
@@ -20,30 +19,26 @@ import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootGraph
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import kotlinx.coroutines.awaitCancellation
-import kotlinx.coroutines.launch
 
 @Destination<RootGraph>
 @Composable
 fun AnimatedVisibilityScope.WebViewSignInScreen(navigator: DestinationsNavigator) = Screen(navigator) {
     val state = rememberWebViewState(url = EhUrl.URL_SIGN_IN)
-    val client = remember {
-        object : AccompanistWebViewClient() {
-            override fun onPageFinished(view: WebView, url: String?) {
-                if (EhCookieStore.hasSignedIn()) {
-                    postLogin()
-                    view.destroy()
-                    launch { bgWork { awaitCancellation() } }
-                }
+    LaunchedEffect(state) {
+        snapshotFlow { !state.isLoading }.collect { hasFinished ->
+            if (hasFinished && EhCookieStore.hasSignedIn()) {
+                postLogin()
+                state.webView?.destroy()
+                bgWork { awaitCancellation() }
             }
         }
-    }
-    SideEffect {
-        EhUtils.signOut()
     }
     WebView(
         state = state,
         modifier = Modifier.fillMaxSize(),
-        onCreated = { it.setDefaultSettings() },
-        client = client,
+        onCreated = {
+            EhUtils.signOut()
+            it.setDefaultSettings()
+        },
     )
 }
