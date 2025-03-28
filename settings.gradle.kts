293a9a60b8e5e412b94612eba9458a525e9f8dfa
FooIbar <118464521+FooIbar@users.noreply.github.com>
2025-03-28 17:00:30 +0800

    Update xmlutil to 0.91.0-RC1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
     }
 }
 
