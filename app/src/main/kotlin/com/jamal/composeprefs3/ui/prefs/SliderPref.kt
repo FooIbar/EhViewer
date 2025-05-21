1ffa468f350dd1631f31a1a73bc690a7aed41b76
FooIbar <118464521+FooIbar@users.noreply.github.com>
2025-05-21 23:39:00 +0800

    Avoid drawing invisible slider ticks and cleanup

--- app/src/main/kotlin/com/jamal/composeprefs3/ui/prefs/SliderPref.kt
+++ app/src/main/kotlin/com/jamal/composeprefs3/ui/prefs/SliderPref.kt
@@ -6,11 +6,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Slider
-import androidx.compose.material3.SliderDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -18,25 +16,24 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.hippo.ehviewer.ui.main.RollingNumber
-import kotlin.math.roundToInt
+import com.hippo.ehviewer.ui.tools.Slider
 
 @Composable
 fun SliderPref(
     title: String,
     modifier: Modifier = Modifier,
     summary: String? = null,
-    defaultValue: Float = 0f,
-    onValueChangeFinished: ((Float) -> Unit)? = null,
-    valueRange: ClosedFloatingPointRange<Float> = 0f..1f,
+    defaultValue: Int = 0,
+    onValueChangeFinished: ((Int) -> Unit)? = null,
+    valueRange: IntRange = 0..1,
     showValue: Boolean = false,
-    showTicks: Boolean = true,
     steps: Int = 0,
     textColor: Color = MaterialTheme.colorScheme.onBackground,
     enabled: Boolean = true,
     leadingIcon: @Composable (() -> Unit)? = null,
-    display: (Float) -> Float = { it },
+    display: (Int) -> Int = { it },
 ) {
-    var value by remember { mutableFloatStateOf(defaultValue) }
+    var value by remember { mutableIntStateOf(defaultValue) }
     Column(
         verticalArrangement = Arrangement.Center,
     ) {
@@ -62,18 +59,10 @@ fun SliderPref(
                 steps = steps,
                 onValueChangeFinished = { onValueChangeFinished?.invoke(value) },
                 enabled = enabled,
-                colors = if (showTicks) {
-                    SliderDefaults.colors()
-                } else {
-                    SliderDefaults.colors(
-                        activeTickColor = Color.Transparent,
-                        inactiveTickColor = Color.Transparent,
-                    )
-                },
             )
             if (showValue) {
                 RollingNumber(
-                    number = display(value).roundToInt(),
+                    number = display(value),
                     color = textColor,
                     modifier = Modifier.weight(0.5f),
                 )
