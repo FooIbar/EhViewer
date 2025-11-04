83eac2f611ad899bcbd76164996b46c08a9d2a8f
FooIbar <118464521+FooIbar@users.noreply.github.com>
2025-11-04 17:09:39 +0800

    AGP 9

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
     alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.builtInKotlin) apply false
     alias(libs.plugins.android.kotlin.multiplatform.library) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.android.lint) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.compose.multiplatform) apply false
-    alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.ksp) apply false
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -28,7 +28,7 @@ dependencyResolutionManagement {
 }
 
 plugins {
-    id("com.android.settings") version "8.13.2"
+    id("com.android.settings") version "9.0.0"
 }
 
 android {
