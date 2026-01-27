706640fcfae33871ef3a7312129adceff5ef3d9c
FooIbar <118464521+FooIbar@users.noreply.github.com>
2026-01-27 20:36:54 +0800

    Fix missing quotes in uploader name

--- app/src/main/kotlin/com/hippo/ehviewer/client/data/ListUrlBuilder.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/client/data/ListUrlBuilder.kt
@@ -35,7 +35,7 @@ data class ListUrlBuilder(
     private var jumpTo: String? = null,
     private var range: Int = 0,
     var category: Int = EhUtils.NONE,
-    private var mKeyword: String? = null,
+    var keyword: String? = null,
     var hash: String? = null,
     var language: Int = -1,
     var advanceSearch: Int = -1,
@@ -69,16 +69,10 @@ data class ListUrlBuilder(
         jumpTo = date
     }
 
-    var keyword: String?
-        get() = if (MODE_UPLOADER == mode) "uploader:$mKeyword" else mKeyword
-        set(keyword) {
-            mKeyword = keyword
-        }
-
     constructor(q: QuickSearch) : this(
         mode = q.mode,
         category = q.category,
-        mKeyword = q.keyword,
+        keyword = q.keyword,
         advanceSearch = q.advanceSearch,
         minRating = q.minRating,
         pageFrom = q.pageFrom,
@@ -90,7 +84,7 @@ data class ListUrlBuilder(
         name = name,
         mode = mode,
         category = category,
-        keyword = mKeyword,
+        keyword = keyword,
         advanceSearch = advanceSearch,
         minRating = minRating,
         pageFrom = pageFrom,
@@ -107,7 +101,7 @@ data class ListUrlBuilder(
         if (q.category != this.category) {
             return false
         }
-        if (q.keyword != mKeyword) {
+        if (q.keyword != keyword) {
             return false
         }
         if (q.advanceSearch != advanceSearch) {
@@ -129,7 +123,7 @@ data class ListUrlBuilder(
         jumpTo = params["seek"],
         range = params["range"]?.toIntOrNull() ?: 0,
         category = params["f_cats"]?.toIntOrNull()?.let(EhUtils::invCategory) ?: EhUtils.NONE,
-        mKeyword = params["f_search"],
+        keyword = params["f_search"],
     ) {
         if (params["advsearch"] == "1") {
             advanceSearch = 0
@@ -160,7 +154,7 @@ data class ListUrlBuilder(
             if (category > 0) {
                 addQueryParameter("f_cats", "${EhUtils.invCategory(category)}")
             }
-            val query = mKeyword?.let { keyword ->
+            val query = keyword?.let { keyword ->
                 if (language == -1 || "gid:" in keyword || "l:" in keyword || "language:" in keyword) {
                     keyword
                 } else {
@@ -214,7 +208,7 @@ data class ListUrlBuilder(
         }.buildString()
         MODE_UPLOADER, MODE_TAG -> {
             val path = if (mode == MODE_UPLOADER) "uploader" else "tag"
-            ehUrl(listOf(path, requireNotNull(mKeyword))) {
+            ehUrl(listOf(path, requireNotNull(keyword))) {
                 addQueryParameterIfNotBlank("prev", prev)
                 addQueryParameterIfNotBlank("next", next)
                 addQueryParameterIfNotBlank("seek", jumpTo)
@@ -226,7 +220,7 @@ data class ListUrlBuilder(
             addQueryParameter("f_shash", requireNotNull(hash))
         }.buildString()
         MODE_TOPLIST -> ehUrl("toplist.php", EhUrl.DOMAIN_E) {
-            addQueryParameter("tl", requireNotNull(mKeyword))
+            addQueryParameter("tl", requireNotNull(keyword))
             addQueryParameterIfNotBlank("p", jumpTo)
         }.buildString()
         else -> throw IllegalStateException("Unexpected value: $mode")
